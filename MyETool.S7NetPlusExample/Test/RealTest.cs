07be00634aee3d7b6d12e222161c0e5950ac2642
bitm <1759743261@qq.com>
2026-06-10 17:12:35 +0800

    feat: 增加批量读写连续的 Real 类型数值的方法以及相关测试

--- MyETool.S7NetPlusExample/Test/RealTest.cs
+++ MyETool.S7NetPlusExample/Test/RealTest.cs
@@ -7,32 +7,62 @@ public static class RealTest
 {
     public static async Task Test(Plc plc)
     {
-        Console.WriteLine("1. 开始同步写 ==========");
-        plc.WriteReal(1, 36, 3.14f);
-        plc.WriteReal(1, 40, 3.15f);
-        plc.WriteReal(1, 44, 3.16f);
-
-        Console.WriteLine("2. 开始同步读 ==========");
-        float float1 = plc.ReadReal(1, 36);
-        float float2 = plc.ReadReal(1, 40);
-        float float3 = plc.ReadReal(1, 44);
-        Console.WriteLine(float1);
-        Console.WriteLine(float2);
-        Console.WriteLine(float3);
+        Console.WriteLine("1. 开始单点同步写 Real");
+        plc.WriteReal(1, 2, 3.11f);
+        plc.WriteReal(1, 6, 3.12f);
+        plc.WriteReal(1, 10, 3.13f);
+        plc.WriteReal(1, 14, 3.14f);
+        plc.WriteReal(1, 18, 3.15f);
+        plc.WriteReal(1, 22, 3.16f);
 
         await Task.Delay(1000);
 
-        Console.WriteLine("3. 开始异步写 ==========");
-        await plc.WriteRealAsync(1, 36, 6.661f);
-        await plc.WriteRealAsync(1, 40, 6.662f);
-        await plc.WriteRealAsync(1, 44, 6.663f);
+        Console.WriteLine("2. 开始单点同步读 Real");
+        Console.WriteLine(plc.ReadReal(1, 2));
+        Console.WriteLine(plc.ReadReal(1, 6));
+        Console.WriteLine(plc.ReadReal(1, 10));
+        Console.WriteLine(plc.ReadReal(1, 14));
+        Console.WriteLine(plc.ReadReal(1, 18));
+        Console.WriteLine(plc.ReadReal(1, 22));
 
-        Console.WriteLine("4. 开始异步读 ==========");
-        float float4 = await plc.ReadRealAsync(1, 36);
-        float float5 = await plc.ReadRealAsync(1, 40);
-        float float6 = await plc.ReadRealAsync(1, 44);
-        Console.WriteLine(float4);
-        Console.WriteLine(float5);
-        Console.WriteLine(float6);
+        await Task.Delay(1000);
+
+        Console.WriteLine("3. 开始连续同步写 Real");
+        plc.WriteReal(1, 2, [4.11f, 4.12f, 4.13f, 4.14f, 4.15f, 4.16f]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("4. 开始连续同步读 Real");
+        Console.WriteLine(string.Join(", ", plc.ReadReal(1, 2, 6)));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("5. 开始单点异步写 Real");
+        await plc.WriteRealAsync(1, 2, 5.11f);
+        await plc.WriteRealAsync(1, 6, 5.12f);
+        await plc.WriteRealAsync(1, 10, 5.13f);
+        await plc.WriteRealAsync(1, 14, 5.14f);
+        await plc.WriteRealAsync(1, 18, 5.15f);
+        await plc.WriteRealAsync(1, 22, 5.16f);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("6. 开始单点异步读 Real");
+        Console.WriteLine(await plc.ReadRealAsync(1, 2));
+        Console.WriteLine(await plc.ReadRealAsync(1, 6));
+        Console.WriteLine(await plc.ReadRealAsync(1, 10));
+        Console.WriteLine(await plc.ReadRealAsync(1, 14));
+        Console.WriteLine(await plc.ReadRealAsync(1, 18));
+        Console.WriteLine(await plc.ReadRealAsync(1, 22));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("7. 开始连续异步写 Real");
+        await plc.WriteRealAsync(1, 2, [6.11f, 6.12f, 6.13f, 6.14f, 6.15f, 6.16f]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("8. 开始连续异步读 Real");
+        Console.WriteLine(string.Join(", ", plc.ReadReal(1, 2, 6)));
     }
 }
--- MyETool.S7NetPlusExample/Util/S7Util.Real.cs
+++ MyETool.S7NetPlusExample/Util/S7Util.Real.cs
@@ -5,32 +5,76 @@ namespace MyETool.S7NetPlusExample.Util;
 
 public static partial class S7Util
 {
-    /// <summary>
-    /// 在 S7 中  Real = 4B = 32b
-    /// 在 C# 中  float = 4B = 32b
-    /// </summary>
-    private const int RealBitLength = 32;
+    private const int RealByteLength = 4;
 
+    /// <summary>
+    /// 【同步】单点读取 Real
+    /// </summary>
     public static float ReadReal(this Plc plc, int db, int byteAdr)
     {
-        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, RealBitLength / 8);
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, RealByteLength);
         float value = Real.FromByteArray(bytes);
         return value;
     }
 
+    /// <summary>
+    /// 【同步】批量读取连续的 Real
+    /// </summary>
+    public static float[] ReadReal(this Plc plc, int db, int byteAdr, int count)
+    {
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, count * RealByteLength);
+        float[] value = Real.ToArray(bytes);
+        return value;
+    }
+
+    /// <summary>
+    /// 【同步】单点写入 Real
+    /// </summary>
     public static void WriteReal(this Plc plc, int db, int byteAdr, float value)
     {
         plc.WriteBytes(DataType.DataBlock, db, byteAdr, Real.ToByteArray(value));
     }
 
+    /// <summary>
+    /// 【同步】批量写入连续的 Real
+    /// </summary>
+    public static void WriteReal(this Plc plc, int db, int byteAdr, float[] value)
+    {
+        plc.WriteBytes(DataType.DataBlock, db, byteAdr, Real.ToByteArray(value));
+    }
+
+    /// <summary>
+    /// 【异步】单点读取 Real
+    /// </summary>
     public static async Task<float> ReadRealAsync(this Plc plc, int db, int byteAdr, CancellationToken cancellationToken = default)
     {
-        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, RealBitLength / 8, cancellationToken);
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, RealByteLength, cancellationToken);
         float value = Real.FromByteArray(bytes);
         return value;
     }
 
+    /// <summary>
+    /// 【异步】批量读取连续的 Real
+    /// </summary>
+    public static async Task<float[]> ReadRealAsync(this Plc plc, int db, int byteAdr, int count, CancellationToken cancellationToken = default)
+    {
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, count * RealByteLength, cancellationToken);
+        float[] value = Real.ToArray(bytes);
+        return value;
+    }
+
+    /// <summary>
+    /// 【异步】单点写入 Real
+    /// </summary>
     public static async Task WriteRealAsync(this Plc plc, int db, int byteAdr, float value, CancellationToken cancellationToken = default)
+    {
+        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, Real.ToByteArray(value), cancellationToken);
+    }
+
+    /// <summary>
+    /// 【异步】批量写入连续的 Real
+    /// </summary>
+    public static async Task WriteRealAsync(this Plc plc, int db, int byteAdr, float[] value, CancellationToken cancellationToken = default)
     {
         await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, Real.ToByteArray(value), cancellationToken);
     }
