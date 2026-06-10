7284bd4b8918bdd5ceeeb0b10e69eb5b11378368
bitm <1759743261@qq.com>
2026-06-10 17:58:27 +0800

    feat: 增加批量读写连续的 LReal 类型数值的方法以及相关测试

--- MyETool.S7NetPlusExample/Test/LRealTest.cs
+++ MyETool.S7NetPlusExample/Test/LRealTest.cs
@@ -7,32 +7,62 @@ public static class LRealTest
 {
     public static async Task Test(Plc plc)
     {
-        Console.WriteLine("1. 开始同步写 ==========");
-        plc.WriteLReal(1, 48, 3.14);
-        plc.WriteLReal(1, 56, 3.15);
-        plc.WriteLReal(1, 64, 3.16);
-
-        Console.WriteLine("2. 开始同步读 ==========");
-        double double1 = plc.ReadLReal(1, 48);
-        double double2 = plc.ReadLReal(1, 56);
-        double double3 = plc.ReadLReal(1, 64);
-        Console.WriteLine(double1);
-        Console.WriteLine(double2);
-        Console.WriteLine(double3);
+        Console.WriteLine("1. 开始单点同步写 LReal");
+        plc.WriteLReal(1, 26, 3.11);
+        plc.WriteLReal(1, 34, 3.12);
+        plc.WriteLReal(1, 42, 3.13);
+        plc.WriteLReal(1, 50, 3.14);
+        plc.WriteLReal(1, 58, 3.15);
+        plc.WriteLReal(1, 66, 3.16);
 
         await Task.Delay(1000);
 
-        Console.WriteLine("3. 开始异步写 ==========");
-        await plc.WriteLRealAsync(1, 48, 8.661);
-        await plc.WriteLRealAsync(1, 56, 8.662);
-        await plc.WriteLRealAsync(1, 64, 8.663);
+        Console.WriteLine("2. 开始单点同步读 LReal");
+        Console.WriteLine(plc.ReadLReal(1, 26));
+        Console.WriteLine(plc.ReadLReal(1, 34));
+        Console.WriteLine(plc.ReadLReal(1, 42));
+        Console.WriteLine(plc.ReadLReal(1, 50));
+        Console.WriteLine(plc.ReadLReal(1, 58));
+        Console.WriteLine(plc.ReadLReal(1, 66));
 
-        Console.WriteLine("4. 开始异步读 ==========");
-        double double4 = await plc.ReadLRealAsync(1, 48);
-        double double5 = await plc.ReadLRealAsync(1, 56);
-        double double6 = await plc.ReadLRealAsync(1, 64);
-        Console.WriteLine(double4);
-        Console.WriteLine(double5);
-        Console.WriteLine(double6);
+        await Task.Delay(1000);
+
+        Console.WriteLine("3. 开始连续同步写 LReal");
+        plc.WriteLReal(1, 26, [4.11, 4.12, 4.13, 4.14, 4.15, 4.16]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("4. 开始连续同步读 LReal");
+        Console.WriteLine(string.Join(", ", plc.ReadLReal(1, 26, 6)));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("5. 开始单点异步写 LReal");
+        await plc.WriteLRealAsync(1, 26, 5.11);
+        await plc.WriteLRealAsync(1, 34, 5.12);
+        await plc.WriteLRealAsync(1, 42, 5.13);
+        await plc.WriteLRealAsync(1, 50, 5.14);
+        await plc.WriteLRealAsync(1, 58, 5.15);
+        await plc.WriteLRealAsync(1, 66, 5.16);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("6. 开始单点异步读 LReal");
+        Console.WriteLine(await plc.ReadLRealAsync(1, 26));
+        Console.WriteLine(await plc.ReadLRealAsync(1, 34));
+        Console.WriteLine(await plc.ReadLRealAsync(1, 42));
+        Console.WriteLine(await plc.ReadLRealAsync(1, 50));
+        Console.WriteLine(await plc.ReadLRealAsync(1, 58));
+        Console.WriteLine(await plc.ReadLRealAsync(1, 66));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("7. 开始连续异步写 LReal");
+        await plc.WriteLRealAsync(1, 26, [6.11, 6.12, 6.13, 6.14, 6.15, 6.16]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("8. 开始连续异步读 LReal");
+        Console.WriteLine(string.Join(", ", await plc.ReadLRealAsync(1, 26, 6)));
     }
 }
--- MyETool.S7NetPlusExample/Util/S7Util.LReal.cs
+++ MyETool.S7NetPlusExample/Util/S7Util.LReal.cs
@@ -5,33 +5,91 @@ namespace MyETool.S7NetPlusExample.Util;
 
 public static partial class S7Util
 {
-    /// <summary>
-    /// 在 S7 中  LReal = 8B = 64b
-    /// 在 C# 中  double = 8B = 64b
-    /// </summary>
-    private const int LRealBitLength = 64;
+    private const int LRealByteLength = 8;
 
+    /// <summary>
+    /// 【同步】单点读取 LReal
+    /// </summary>
     public static double ReadLReal(this Plc plc, int db, int byteAdr)
     {
-        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, LRealBitLength / 8);
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, LRealByteLength);
         double value = LReal.FromByteArray(bytes);
         return value;
     }
 
+    /// <summary>
+    /// 【同步】批量读取连续的 LReal
+    /// </summary>
+    public static double[] ReadLReal(this Plc plc, int db, int byteAdr, int count)
+    {
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, LRealByteLength * count);
+        double[] value = LReal.ToArray(bytes);
+        return value;
+    }
+
+    /// <summary>
+    /// 【同步】单点写入 LReal
+    /// </summary>
     public static void WriteLReal(this Plc plc, int db, int byteAdr, double value)
     {
         plc.WriteBytes(DataType.DataBlock, db, byteAdr, LReal.ToByteArray(value));
     }
 
+    /// <summary>
+    /// 【同步】批量写入连续的 LReal
+    /// </summary>
+    public static void WriteLReal(this Plc plc, int db, int byteAdr, double[] values)
+    {
+        List<byte> list = [];
+        foreach (var value in values)
+        {
+            byte[] array1 = LReal.ToByteArray(value);
+            list.AddRange(array1);
+        }
+
+        plc.WriteBytes(DataType.DataBlock, db, byteAdr, list.ToArray());
+    }
+
+    /// <summary>
+    /// 【异步】单点读取 LReal
+    /// </summary>
     public static async Task<double> ReadLRealAsync(this Plc plc, int db, int byteAdr, CancellationToken cancellationToken = default)
     {
-        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, LRealBitLength / 8, cancellationToken);
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, LRealByteLength, cancellationToken);
         double value = LReal.FromByteArray(bytes);
         return value;
     }
 
+    /// <summary>
+    /// 【异步】批量读取连续的 LReal
+    /// </summary>
+    public static async Task<double[]> ReadLRealAsync(this Plc plc, int db, int byteAdr, int count, CancellationToken cancellationToken = default)
+    {
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, LRealByteLength * count, cancellationToken);
+        double[] value = LReal.ToArray(bytes);
+        return value;
+    }
+
+    /// <summary>
+    /// 【异步】单点写入 LReal
+    /// </summary>
     public static async Task WriteLRealAsync(this Plc plc, int db, int byteAdr, double value, CancellationToken cancellationToken = default)
     {
         await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, LReal.ToByteArray(value), cancellationToken);
+    }
+
+    /// <summary>
+    /// 【异步】批量写入连续的 LReal
+    /// </summary>
+    public static async Task WriteLRealAsync(this Plc plc, int db, int byteAdr, double[] values, CancellationToken cancellationToken = default)
+    {
+        List<byte> list = [];
+        foreach (var value in values)
+        {
+            byte[] array1 = LReal.ToByteArray(value);
+            list.AddRange(array1);
+        }
+
+        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, list.ToArray(), cancellationToken);
     }
 }
