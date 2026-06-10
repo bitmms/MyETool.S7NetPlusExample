65d94c91160ed68b41b3cc7269055dc6bf2edff7
bitm <1759743261@qq.com>
2026-06-10 10:40:32 +0800

    refactor: 修改 String 类型仅支持 ASCII 编码读写

--- MyETool.S7NetPlusExample/Test/StringTest.cs
+++ MyETool.S7NetPlusExample/Test/StringTest.cs
@@ -1,5 +1,4 @@
-﻿using System.Text;
-using MyETool.S7NetPlusExample.Util;
+﻿using MyETool.S7NetPlusExample.Util;
 using S7.Net;
 
 namespace MyETool.S7NetPlusExample.Test;
@@ -21,41 +20,24 @@ public static class StringTest
         Console.WriteLine($"PLC 连接成功：{plcIp}");
 
 
-        // 内存中使用：Encoding.Unicode，编码格式一致，节省时间
-        // 存储中使用：Encoding.UTF8，动态编码长度，节省空间
-        Encoding encoding = Encoding.Unicode;
-
-
         Console.WriteLine("1. 开始同步写 ==========");
-        plc.WriteString(1, 84, "今天开始我要自己上厕所", 254, encoding);
-        plc.WriteString(1, 340, "world", 254, encoding);
-        plc.WriteString(1, 596, "宝宝巴士教我上厕所秘诀", 254, encoding);
-        plc.WriteString(1, 852, "abcdefg123456789", 10, encoding);
-        plc.WriteString(1, 864, "白日依山尽，黄河入海流。欲穷千里目，更上一层楼。", 20, encoding);
-        plc.WriteString(1, 886, "待到山花烂漫时，她在丛中笑。", 254, encoding);
+        plc.WriteString(1, 84, "Nothing is impossible...", 254);
+        plc.WriteString(1, 596, "abcdefg1234567", 254);
+        plc.WriteString(1, 852, "qwertyuiop", 10);
 
         Console.WriteLine("2. 开始同步读 ==========");
-        Console.WriteLine(plc.ReadString(1, 84, 256, encoding));
-        Console.WriteLine(plc.ReadString(1, 340, 254, encoding));
-        Console.WriteLine(plc.ReadString(1, 596, 254, encoding));
-        Console.WriteLine(plc.ReadString(1, 852, 10, encoding));
-        Console.WriteLine(plc.ReadString(1, 864, 20, encoding));
-        Console.WriteLine(plc.ReadString(1, 886, 254, encoding));
+        Console.WriteLine(plc.ReadString(1, 84, 256));
+        Console.WriteLine(plc.ReadString(1, 596, 254));
+        Console.WriteLine(plc.ReadString(1, 852, 10));
 
         Console.WriteLine("3. 开始异步写 ==========");
-        await plc.WriteStringAsync(1, 84, "从明天起，做一个幸福的人", 254, encoding);
-        await plc.WriteStringAsync(1, 340, "喂马、劈柴，周游世界", 254, encoding);
-        await plc.WriteStringAsync(1, 596, "从明天起，关心粮食和蔬菜", 254, encoding);
-        await plc.WriteStringAsync(1, 852, "我有一所房子，面朝大海，春暖花开", 10, encoding);
-        await plc.WriteStringAsync(1, 864, "从明天起，和每一个亲人通信", 20, encoding);
-        await plc.WriteStringAsync(1, 886, "告诉他们我的幸福", 254, encoding);
+        await plc.WriteStringAsync(1, 84, "Goodbye", 254);
+        await plc.WriteStringAsync(1, 596, "You're welcome", 254);
+        await plc.WriteStringAsync(1, 852, "Every day is new", 10);
 
         Console.WriteLine("4. 开始异步读 ==========");
-        Console.WriteLine(await plc.ReadStringAsync(1, 84, 256, encoding));
-        Console.WriteLine(await plc.ReadStringAsync(1, 340, 254, encoding));
-        Console.WriteLine(await plc.ReadStringAsync(1, 596, 254, encoding));
-        Console.WriteLine(await plc.ReadStringAsync(1, 852, 10, encoding));
-        Console.WriteLine(await plc.ReadStringAsync(1, 864, 20, encoding));
-        Console.WriteLine(await plc.ReadStringAsync(1, 886, 254, encoding));
+        Console.WriteLine(await plc.ReadStringAsync(1, 84, 256));
+        Console.WriteLine(await plc.ReadStringAsync(1, 596, 254));
+        Console.WriteLine(await plc.ReadStringAsync(1, 852, 10));
     }
 }
--- MyETool.S7NetPlusExample/Util/S7Util.String.cs
+++ MyETool.S7NetPlusExample/Util/S7Util.String.cs
@@ -6,13 +6,18 @@ namespace MyETool.S7NetPlusExample.Util;
 public static partial class S7Util
 {
     /*
-     * 在 S7 中，分配字符串采用：string 或 string[n]
+     * 在 S7 中，分配 String 采用：String 或 String[n]
      *      * 这里 n 大于等于1 且 小于等于 254，默认是 254
-     *      * 这里的 n 表示可写入内容的字节数
-     *      * PLC 实际分配的字节数是 1 + 1 + n，需要两个头字节填充长度：最大内容字节数 + 实际内容字节数
+     *      * 这里的 n 表示可写入内容的字符数量
+     *      * PLC 实际分配的字节数是 1 + 1 + n*1，需要2个头字节填充长度：最大写入字符数量 + 实际写入字符数量
+     *      * PLC String 按照 Encoding.ASCII 处理单字节字符
+     *      * PLC String 永远按 1 字符 = 1 字节 计算
+     *      * PLC String 只支持单字节字符，只用 1 字节存储字符，包括头字节的长度也用 1 字节存储
      */
 
-    public static string ReadString(this Plc plc, int db, int byteAdr, int n, Encoding s7Encoding)
+    private static readonly Encoding S7Encoding = Encoding.ASCII;
+
+    public static string ReadString(this Plc plc, int db, int byteAdr, int n)
     {
         if (n <= 0) return "";
         n = Math.Min(254, n);
@@ -23,13 +28,13 @@ public static partial class S7Util
         int contentLength = bytes[1]; // 实际写入字符串的字节数
         if (totalLength < contentLength) return ""; // 保险处理，避免之前针对 plc 的操作出现问题
 
-        string fromByteArray = s7Encoding.GetString(bytes, 2, contentLength);
+        string fromByteArray = S7Encoding.GetString(bytes, 2, contentLength);
         return fromByteArray;
     }
 
-    public static void WriteString(this Plc plc, int db, int byteAdr, string value, byte n, Encoding s7Encoding)
+    public static void WriteString(this Plc plc, int db, int byteAdr, string value, byte n)
     {
-        byte[] bytes = s7Encoding.GetBytes(value);
+        byte[] bytes = S7Encoding.GetBytes(value);
         n = Math.Min(n, (byte)254);
         n = Math.Max(n, (byte)0);
         byte[] destArray = new byte[2 + n];
@@ -45,24 +50,22 @@ public static partial class S7Util
     /// <param name="plc">plc实例</param>
     /// <param name="db">DB号</param>
     /// <param name="byteAdr">起始地址</param>
-    /// <param name="n">声明 string 时，string[n]，这里的 n 是实际可写入字符串的字节长度，在 plc 中分配的空间字节实际是 2+n</param>
-    /// <param name="s7Encoding">字符串编码</param>
+    /// <param name="maxCharCount">声明 string 时，string[n]，这里的 n 是实际可写入的字符数量</param>
     /// <param name="cancellationToken">取消异步的 token</param>
     /// <returns></returns>
-    public static async Task<string> ReadStringAsync(this Plc plc, int db, int byteAdr, int n, Encoding s7Encoding, CancellationToken cancellationToken = default)
+    public static async Task<string> ReadStringAsync(this Plc plc, int db, int byteAdr, int maxCharCount, CancellationToken cancellationToken = default)
     {
-        if (n <= 0) return "";
-        n = Math.Min(254, n);
+        if (maxCharCount <= 0) return "";
+        maxCharCount = Math.Min(254, maxCharCount);
 
-        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, 2 + n, cancellationToken); // 这里保证了至少读取 2 个字节的数据
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, 2 + maxCharCount, cancellationToken); // 这里保证了至少读取 2 个字节的数据
 
         int totalLength = bytes[0]; // 实际可写入字符串的字节数
         int contentLength = bytes[1]; // 实际写入字符串的字节数
-        Console.WriteLine(contentLength);
         if (totalLength < contentLength) return ""; // 保险处理，避免之前针对 plc 的操作出现问题
 
-        string fromByteArray = s7Encoding.GetString(bytes, 2, contentLength);
-        return fromByteArray;
+        string resultString = S7Encoding.GetString(bytes, 2, contentLength);
+        return resultString;
     }
 
     /// <summary>
@@ -72,22 +75,21 @@ public static partial class S7Util
     /// <param name="db">DB号</param>
     /// <param name="byteAdr">起始地址</param>
     /// <param name="value">写入的字符串</param>
-    /// <param name="n">声明 string 时，string[n]，这里的 n 是实际可写入字符串的字节长度，在 plc 中分配的空间字节实际是 2+n</param>
-    /// <param name="s7Encoding">字符串编码</param>
+    /// <param name="maxCharCount">声明 string 时，string[n]，这里的 n 是实际可写入的字符数量</param>
     /// <param name="cancellationToken">取消异步的 token</param>
     /// <returns></returns>
-    public static async Task WriteStringAsync(this Plc plc, int db, int byteAdr, string value, byte n, Encoding s7Encoding, CancellationToken cancellationToken = default)
+    public static async Task WriteStringAsync(this Plc plc, int db, int byteAdr, string value, byte maxCharCount, CancellationToken cancellationToken = default)
     {
-        byte[] bytes = s7Encoding.GetBytes(value);
+        byte[] bytes = S7Encoding.GetBytes(value);
         // 1. 实际可写入字符串的字节数的范围是 [0, 254]
-        n = Math.Min(n, (byte)254);
-        n = Math.Max(n, (byte)0);
+        maxCharCount = Math.Min(maxCharCount, (byte)254);
+        maxCharCount = Math.Max(maxCharCount, (byte)0);
         // 2. 声明实际写入 PLC 的字节数据：2+n
-        byte[] destArray = new byte[2 + n];
+        byte[] destArray = new byte[2 + maxCharCount];
         // 3. 第一个字节是：实际可写入字符串的字节数
-        destArray[0] = n;
+        destArray[0] = maxCharCount;
         // 4. 第二个字节是：实际写入字符串的字节数
-        destArray[1] = Math.Min((byte)bytes.Length, n); // 即使字符串的长度很长，这里自动截断到 n
+        destArray[1] = Math.Min((byte)bytes.Length, maxCharCount); // 即使字符串的长度很长，这里自动截断到 n
         // 5. 填充剩余字节
         Array.Copy(bytes, 0, destArray, 2, destArray[1]);
         // 6. 写入 plc
