43cffc0d13eee7a58178ea231aabe72ac8e81655
bitm <1759743261@qq.com>
2026-06-10 18:11:49 +0800

    feat: 增加批量读写连续的 UInt 类型数值的方法以及相关测试

--- MyETool.S7NetPlusExample/Test/UIntTest.cs
+++ MyETool.S7NetPlusExample/Test/UIntTest.cs
@@ -7,32 +7,62 @@ public static class UIntTest
 {
     public static async Task Test(Plc plc)
     {
-        Console.WriteLine("1. 开始同步写 ==========");
-        plc.WriteUInt(1, 0, 31);
-        plc.WriteUInt(1, 2, 32);
-        plc.WriteUInt(1, 4, 33);
-
-        Console.WriteLine("2. 开始同步读 ==========");
-        uint b1 = plc.ReadUInt(1, 0);
-        uint b2 = plc.ReadUInt(1, 2);
-        uint b3 = plc.ReadUInt(1, 4);
-        Console.WriteLine(b1);
-        Console.WriteLine(b2);
-        Console.WriteLine(b3);
+        Console.WriteLine("1. 开始单点同步写 UInt");
+        plc.WriteUInt(1, 74, 11);
+        plc.WriteUInt(1, 76, 12);
+        plc.WriteUInt(1, 78, 13);
+        plc.WriteUInt(1, 80, 14);
+        plc.WriteUInt(1, 82, 15);
+        plc.WriteUInt(1, 84, 16);
 
         await Task.Delay(1000);
 
-        Console.WriteLine("3. 开始异步写 ==========");
-        await plc.WriteUIntAsync(1, 0, 61);
-        await plc.WriteUIntAsync(1, 2, 62);
-        await plc.WriteUIntAsync(1, 4, 63);
+        Console.WriteLine("2. 开始单点同步读 UInt");
+        Console.WriteLine(plc.ReadUInt(1, 74));
+        Console.WriteLine(plc.ReadUInt(1, 76));
+        Console.WriteLine(plc.ReadUInt(1, 78));
+        Console.WriteLine(plc.ReadUInt(1, 80));
+        Console.WriteLine(plc.ReadUInt(1, 82));
+        Console.WriteLine(plc.ReadUInt(1, 84));
 
-        Console.WriteLine("4. 开始异步读 ==========");
-        uint b4 = await plc.ReadUIntAsync(1, 0);
-        uint b5 = await plc.ReadUIntAsync(1, 2);
-        uint b6 = await plc.ReadUIntAsync(1, 4);
-        Console.WriteLine(b4);
-        Console.WriteLine(b5);
-        Console.WriteLine(b6);
+        await Task.Delay(1000);
+
+        Console.WriteLine("3. 开始连续同步写 UInt");
+        plc.WriteUInt(1, 74, [21, 22, 23, 24, 25, 26]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("4. 开始连续同步读 UInt");
+        Console.WriteLine(string.Join(", ", plc.ReadUInt(1, 74, 6)));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("5. 开始单点异步写 UInt");
+        await plc.WriteUIntAsync(1, 74, 31);
+        await plc.WriteUIntAsync(1, 76, 32);
+        await plc.WriteUIntAsync(1, 78, 33);
+        await plc.WriteUIntAsync(1, 80, 34);
+        await plc.WriteUIntAsync(1, 82, 35);
+        await plc.WriteUIntAsync(1, 84, 36);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("6. 开始单点异步读 UInt");
+        Console.WriteLine(await plc.ReadUIntAsync(1, 74));
+        Console.WriteLine(await plc.ReadUIntAsync(1, 76));
+        Console.WriteLine(await plc.ReadUIntAsync(1, 78));
+        Console.WriteLine(await plc.ReadUIntAsync(1, 80));
+        Console.WriteLine(await plc.ReadUIntAsync(1, 82));
+        Console.WriteLine(await plc.ReadUIntAsync(1, 84));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("7. 开始连续异步写 UInt");
+        await plc.WriteUIntAsync(1, 74, [41, 42, 43, 44, 45, 46]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("8. 开始连续异步读 UInt");
+        Console.WriteLine(string.Join(", ", await plc.ReadUIntAsync(1, 74, 6)));
     }
 }
--- MyETool.S7NetPlusExample/Util/S7Util.UInt.cs
+++ MyETool.S7NetPlusExample/Util/S7Util.UInt.cs
@@ -5,32 +5,76 @@ namespace MyETool.S7NetPlusExample.Util;
 
 public static partial class S7Util
 {
-    /// <summary>
-    /// 在 S7 中  UInt = 2B = 16b
-    /// 在 C# 中  UInt16 = ushort = 2B = 16b
-    /// </summary>
-    private const int UIntBitLength = 16;
+    private const int UIntByteLength = 2;
 
+    /// <summary>
+    /// 【同步】单点读取 UInt
+    /// </summary>
     public static ushort ReadUInt(this Plc plc, int db, int byteAdr)
     {
-        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, UIntBitLength / 8);
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, UIntByteLength);
         ushort value = Word.FromByteArray(bytes);
         return value;
     }
 
+    /// <summary>
+    /// 【同步】批量读取连续的 UInt
+    /// </summary>
+    public static ushort[] ReadUInt(this Plc plc, int db, int byteAdr, int count)
+    {
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, UIntByteLength * count);
+        ushort[] value = Word.ToArray(bytes);
+        return value;
+    }
+
+    /// <summary>
+    /// 【同步】单点写入 UInt
+    /// </summary>
     public static void WriteUInt(this Plc plc, int db, int byteAdr, ushort value)
     {
         plc.WriteBytes(DataType.DataBlock, db, byteAdr, Word.ToByteArray(value));
     }
 
+    /// <summary>
+    /// 【同步】批量写入连续的 UInt
+    /// </summary>
+    public static void WriteUInt(this Plc plc, int db, int byteAdr, ushort[] value)
+    {
+        plc.WriteBytes(DataType.DataBlock, db, byteAdr, Word.ToByteArray(value));
+    }
+
+    /// <summary>
+    /// 【异步】单点读取 UInt
+    /// </summary>
     public static async Task<ushort> ReadUIntAsync(this Plc plc, int db, int byteAdr, CancellationToken cancellationToken = default)
     {
-        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, UIntBitLength / 8, cancellationToken);
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, UIntByteLength, cancellationToken);
         ushort value = Word.FromByteArray(bytes);
         return value;
     }
 
+    /// <summary>
+    /// 【异步】批量读取连续的 UInt
+    /// </summary>
+    public static async Task<ushort[]> ReadUIntAsync(this Plc plc, int db, int byteAdr, int count, CancellationToken cancellationToken = default)
+    {
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, UIntByteLength * count, cancellationToken);
+        ushort[] value = Word.ToArray(bytes);
+        return value;
+    }
+
+    /// <summary>
+    /// 【异步】单点写入 UInt
+    /// </summary>
     public static async Task WriteUIntAsync(this Plc plc, int db, int byteAdr, ushort value, CancellationToken cancellationToken = default)
+    {
+        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, Word.ToByteArray(value), cancellationToken);
+    }
+
+    /// <summary>
+    /// 【异步】批量写入连续的 UInt
+    /// </summary>
+    public static async Task WriteUIntAsync(this Plc plc, int db, int byteAdr, ushort[] value, CancellationToken cancellationToken = default)
     {
         await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, Word.ToByteArray(value), cancellationToken);
     }
