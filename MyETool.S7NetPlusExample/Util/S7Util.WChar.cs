a76e1179421c77a5479e77c01daaebc9dd876710
bitm <1759743261@qq.com>
2026-06-10 15:15:19 +0800

    docs: 补充对 WChar 读写时关于大端、小端问题的注释

--- MyETool.S7NetPlusExample/Util/S7Util.WChar.cs
+++ MyETool.S7NetPlusExample/Util/S7Util.WChar.cs
@@ -26,14 +26,19 @@ public static partial class S7Util
 
     public static async Task<char> ReadWCharAsync(this Plc plc, int db, int byteAdr, CancellationToken cancellationToken = default)
     {
+        // 1. 从 PLC 读取双字节字符，此时是 Unicode UTF-16 双字节大端
         byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, WCharBitLength / 2, cancellationToken);
+        // 2. 利用 Unicode 大端进行解码得到 char 数据
         char resultChar = Encoding.BigEndianUnicode.GetChars(bytes)[0];
+        // 3. 返回
         return resultChar;
     }
 
     public static async Task WriteWCharAsync(this Plc plc, int db, int byteAdr, char value, CancellationToken cancellationToken = default)
     {
+        // 1. 利用 Unicode 大端进行解码，得到 Unicode UTF-16 大端双字节数组
         byte[] bytes = Encoding.BigEndianUnicode.GetBytes([value]);
+        // 2. 写入 PLC 中
         await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, bytes, cancellationToken);
     }
 }
