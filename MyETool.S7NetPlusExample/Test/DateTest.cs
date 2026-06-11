ba102b55b1b82b31ad3dd6ab74b936f56cf454fe
bitm <1759743261@qq.com>
2026-06-11 14:36:36 +0800

    feat: 增加批量读写连续的 Date 类型数值的方法以及相关测试

--- MyETool.S7NetPlusExample/Test/DateTest.cs
+++ MyETool.S7NetPlusExample/Test/DateTest.cs
@@ -7,26 +7,76 @@ public static class DateTest
 {
     public static async Task Test(Plc plc)
     {
-        Console.WriteLine("1. 开始同步写 ==========");
-        plc.WriteDate(1, 2176, new DateOnly(1990, 1, 5));
-        plc.WriteDate(1, 2178, new DateOnly(1990, 2, 5));
-        plc.WriteDate(1, 2180, new DateOnly(1990, 3, 5));
-
-        Console.WriteLine("2. 开始同步读 ==========");
-        Console.WriteLine(plc.ReadDate(1, 2176));
-        Console.WriteLine(plc.ReadDate(1, 2178));
-        Console.WriteLine(plc.ReadDate(1, 2180));
+        Console.WriteLine("1. 开始单点同步写 Date");
+        plc.WriteDate(1, 218, new DateOnly(2026, 1, 11));
+        plc.WriteDate(1, 220, new DateOnly(2026, 2, 12));
+        plc.WriteDate(1, 222, new DateOnly(2026, 3, 13));
+        plc.WriteDate(1, 224, new DateOnly(2026, 4, 14));
+        plc.WriteDate(1, 226, new DateOnly(2026, 5, 15));
+        plc.WriteDate(1, 228, new DateOnly(2026, 6, 16));
 
         await Task.Delay(1000);
 
-        Console.WriteLine("3. 开始异步写 ==========");
-        await plc.WriteDateAsync(1, 2176, new DateOnly(1991, 1, 5));
-        await plc.WriteDateAsync(1, 2178, new DateOnly(1991, 2, 5));
-        await plc.WriteDateAsync(1, 2180, new DateOnly(1991, 3, 5));
+        Console.WriteLine("2. 开始单点同步读 Date");
+        Console.WriteLine(plc.ReadDate(1, 218));
+        Console.WriteLine(plc.ReadDate(1, 220));
+        Console.WriteLine(plc.ReadDate(1, 222));
+        Console.WriteLine(plc.ReadDate(1, 224));
+        Console.WriteLine(plc.ReadDate(1, 226));
+        Console.WriteLine(plc.ReadDate(1, 228));
 
-        Console.WriteLine("4. 开始异步读 ==========");
-        Console.WriteLine(await plc.ReadDateAsync(1, 2176));
-        Console.WriteLine(await plc.ReadDateAsync(1, 2178));
-        Console.WriteLine(await plc.ReadDateAsync(1, 2180));
+        await Task.Delay(1000);
+
+        Console.WriteLine("3. 开始连续同步写 Date");
+        plc.WriteDate(1, 218, [
+            new DateOnly(2026, 3, 21),
+            new DateOnly(2026, 3, 22),
+            new DateOnly(2026, 3, 23),
+            new DateOnly(2026, 3, 24),
+            new DateOnly(2026, 3, 25),
+            new DateOnly(2026, 3, 26),
+        ]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("4. 开始连续同步读 Date");
+        Console.WriteLine(string.Join(", ", plc.ReadDate(1, 218, 6)));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("5. 开始单点异步写 Date");
+        await plc.WriteDateAsync(1, 218, new DateOnly(2028, 1, 11));
+        await plc.WriteDateAsync(1, 220, new DateOnly(2028, 2, 12));
+        await plc.WriteDateAsync(1, 222, new DateOnly(2028, 3, 13));
+        await plc.WriteDateAsync(1, 224, new DateOnly(2028, 4, 14));
+        await plc.WriteDateAsync(1, 226, new DateOnly(2028, 5, 15));
+        await plc.WriteDateAsync(1, 228, new DateOnly(2028, 6, 16));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("6. 开始单点异步读 Date");
+        Console.WriteLine(await plc.ReadDateAsync(1, 218));
+        Console.WriteLine(await plc.ReadDateAsync(1, 220));
+        Console.WriteLine(await plc.ReadDateAsync(1, 222));
+        Console.WriteLine(await plc.ReadDateAsync(1, 224));
+        Console.WriteLine(await plc.ReadDateAsync(1, 226));
+        Console.WriteLine(await plc.ReadDateAsync(1, 228));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("7. 开始连续异步写 Date");
+        await plc.WriteDateAsync(1, 218, [
+            new DateOnly(2026, 9, 21),
+            new DateOnly(2026, 9, 22),
+            new DateOnly(2026, 9, 23),
+            new DateOnly(2026, 9, 24),
+            new DateOnly(2026, 9, 25),
+            new DateOnly(2026, 9, 26),
+        ]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("8. 开始连续异步读 Date");
+        Console.WriteLine(string.Join(", ", await plc.ReadDateAsync(1, 218, 6)));
     }
 }
--- MyETool.S7NetPlusExample/Util/S7Util.Date.cs
+++ MyETool.S7NetPlusExample/Util/S7Util.Date.cs
@@ -4,15 +4,14 @@ namespace MyETool.S7NetPlusExample.Util;
 
 public static partial class S7Util
 {
-    /// <summary>
-    /// 在 S7 中  Date = 2B = 16b，存的是「从 1990-01-01 算起的天数」，存储的是一个数值
-    /// 在 C# 中  UInt16 = ushort = 2B = 16b --->>> DateOnly 或者 DateTime
-    /// </summary>
-    private const int DateBitLength = 16;
+    private const int DateByteLength = 2;
 
+    /// <summary>
+    /// 【同步】单点读取 Date
+    /// </summary>
     public static DateOnly ReadDate(this Plc plc, int db, int byteAdr)
     {
-        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, DateBitLength / 8);
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, DateByteLength);
         // 大端转天数
         ushort days = (ushort)((bytes[0] << 8) | bytes[1]);
         // 转 DateOnly（只有日期，无时间）
@@ -21,6 +20,31 @@ public static partial class S7Util
         return plcDate;
     }
 
+    /// <summary>
+    /// 【同步】批量读取连续的 Date
+    /// </summary>
+    public static DateOnly[] ReadDate(this Plc plc, int db, int byteAdr, int count)
+    {
+        DateOnly[] dateOnlyArray = new DateOnly[count];
+
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, DateByteLength * count);
+
+        for (int i = 0; i < count * 2; i += 2)
+        {
+            // 大端转天数
+            ushort days = (ushort)((bytes[i] << 8) | bytes[i + 1]);
+            // 转 DateOnly（只有日期，无时间）
+            DateOnly baseDate = new DateOnly(1990, 1, 1);
+            DateOnly plcDate = baseDate.AddDays(days);
+            dateOnlyArray[i / 2] = plcDate;
+        }
+
+        return dateOnlyArray;
+    }
+
+    /// <summary>
+    /// 【同步】单点写入 Date
+    /// </summary>
     public static void WriteDate(this Plc plc, int db, int byteAdr, DateOnly value)
     {
         // 1. 计算天数
@@ -34,9 +58,36 @@ public static partial class S7Util
         plc.WriteBytes(DataType.DataBlock, db, byteAdr, bytes);
     }
 
+    /// <summary>
+    /// 【同步】批量写入连续的 Date
+    /// </summary>
+    public static void WriteDate(this Plc plc, int db, int byteAdr, DateOnly[] values)
+    {
+        int idx = 0;
+        byte[] results = new byte[values.Length * DateByteLength];
+        foreach (var value in values)
+        {
+            // 1. 计算天数
+            DateOnly baseDate = new DateOnly(1990, 1, 1);
+            ushort days = (ushort)(value.DayNumber - baseDate.DayNumber);
+            // 2. 大端格式
+            byte[] bytes = new byte[2];
+            bytes[0] = (byte)(days >> 8);
+            bytes[1] = (byte)(days & 0xFF);
+            // 3. 填充
+            results[idx++] = bytes[0];
+            results[idx++] = bytes[1];
+        }
+
+        plc.WriteBytes(DataType.DataBlock, db, byteAdr, results);
+    }
+
+    /// <summary>
+    /// 【异步】单点读取 Date
+    /// </summary>
     public static async Task<DateOnly> ReadDateAsync(this Plc plc, int db, int byteAdr, CancellationToken cancellationToken = default)
     {
-        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, DateBitLength / 8, cancellationToken);
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, DateByteLength, cancellationToken);
         // 大端转天数
         ushort days = (ushort)((bytes[0] << 8) | bytes[1]);
         // 转 DateOnly（只有日期，无时间）
@@ -45,6 +96,31 @@ public static partial class S7Util
         return plcDate;
     }
 
+    /// <summary>
+    /// 【异步】批量读取连续的 Date
+    /// </summary>
+    public static async Task<DateOnly[]> ReadDateAsync(this Plc plc, int db, int byteAdr, int count, CancellationToken cancellationToken = default)
+    {
+        DateOnly[] dateOnlyArray = new DateOnly[count];
+
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, DateByteLength * count, cancellationToken);
+
+        for (int i = 0; i < count * 2; i += 2)
+        {
+            // 大端转天数
+            ushort days = (ushort)((bytes[i] << 8) | bytes[i + 1]);
+            // 转 DateOnly（只有日期，无时间）
+            DateOnly baseDate = new DateOnly(1990, 1, 1);
+            DateOnly plcDate = baseDate.AddDays(days);
+            dateOnlyArray[i / 2] = plcDate;
+        }
+
+        return dateOnlyArray;
+    }
+
+    /// <summary>
+    /// 【异步】单点写入 Date
+    /// </summary>
     public static async Task WriteDateAsync(this Plc plc, int db, int byteAdr, DateOnly value, CancellationToken cancellationToken = default)
     {
         // 1. 计算天数
@@ -56,5 +132,29 @@ public static partial class S7Util
         bytes[1] = (byte)(days & 0xFF);
         // 3. 写入PLC
         await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, bytes, cancellationToken);
+    }
+
+    /// <summary>
+    /// 【异步】批量写入连续的 Date
+    /// </summary>
+    public static async Task WriteDateAsync(this Plc plc, int db, int byteAdr, DateOnly[] values, CancellationToken cancellationToken = default)
+    {
+        int idx = 0;
+        byte[] results = new byte[values.Length * DateByteLength];
+        foreach (var value in values)
+        {
+            // 1. 计算天数
+            DateOnly baseDate = new DateOnly(1990, 1, 1);
+            ushort days = (ushort)(value.DayNumber - baseDate.DayNumber);
+            // 2. 大端格式
+            byte[] bytes = new byte[2];
+            bytes[0] = (byte)(days >> 8);
+            bytes[1] = (byte)(days & 0xFF);
+            // 3. 填充
+            results[idx++] = bytes[0];
+            results[idx++] = bytes[1];
+        }
+
+        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, results, cancellationToken);
     }
 }
