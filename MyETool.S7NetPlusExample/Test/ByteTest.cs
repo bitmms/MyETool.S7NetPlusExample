31830694b31b03e4815dd299deb780f251d05f6a
bitm <1759743261@qq.com>
2026-06-11 09:49:10 +0800

    feat: 增加批量读写连续的 Byte 类型数值的方法以及相关测试

--- MyETool.S7NetPlusExample/Test/ByteTest.cs
+++ MyETool.S7NetPlusExample/Test/ByteTest.cs
@@ -7,32 +7,62 @@ public static class ByteTest
 {
     public static async Task Test(Plc plc)
     {
-        Console.WriteLine("1. 开始同步写 ==========");
-        plc.WriteByte(1, 486, 31);
-        plc.WriteByte(1, 487, 32);
-        plc.WriteByte(1, 488, 33);
-
-        Console.WriteLine("2. 开始同步读 ==========");
-        byte b1 = plc.ReadByte(1, 486);
-        byte b2 = plc.ReadByte(1, 487);
-        byte b3 = plc.ReadByte(1, 488);
-        Console.WriteLine(b1);
-        Console.WriteLine(b2);
-        Console.WriteLine(b3);
+        Console.WriteLine("1. 开始单点同步写 Byte");
+        plc.WriteByte(1, 158, 101);
+        plc.WriteByte(1, 159, 102);
+        plc.WriteByte(1, 160, 103);
+        plc.WriteByte(1, 161, 104);
+        plc.WriteByte(1, 162, 105);
+        plc.WriteByte(1, 163, 106);
 
         await Task.Delay(1000);
 
-        Console.WriteLine("3. 开始异步写 ==========");
-        await plc.WriteByteAsync(1, 486, 61);
-        await plc.WriteByteAsync(1, 487, 62);
-        await plc.WriteByteAsync(1, 488, 63);
+        Console.WriteLine("2. 开始单点同步读 Byte");
+        Console.WriteLine(plc.ReadByte(1, 158));
+        Console.WriteLine(plc.ReadByte(1, 159));
+        Console.WriteLine(plc.ReadByte(1, 160));
+        Console.WriteLine(plc.ReadByte(1, 161));
+        Console.WriteLine(plc.ReadByte(1, 162));
+        Console.WriteLine(plc.ReadByte(1, 163));
 
-        Console.WriteLine("4. 开始异步读 ==========");
-        byte b4 = await plc.ReadByteAsync(1, 486);
-        byte b5 = await plc.ReadByteAsync(1, 487);
-        byte b6 = await plc.ReadByteAsync(1, 488);
-        Console.WriteLine(b4);
-        Console.WriteLine(b5);
-        Console.WriteLine(b6);
+        await Task.Delay(1000);
+
+        Console.WriteLine("3. 开始连续同步写 Byte");
+        plc.WriteByte(1, 158, [111, 112, 113, 114, 115, 116]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("4. 开始连续同步读 Byte");
+        Console.WriteLine(string.Join(", ", plc.ReadByte(1, 158, 6)));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("5. 开始单点异步写 Byte");
+        await plc.WriteByteAsync(1, 158, 121);
+        await plc.WriteByteAsync(1, 159, 122);
+        await plc.WriteByteAsync(1, 160, 123);
+        await plc.WriteByteAsync(1, 161, 124);
+        await plc.WriteByteAsync(1, 162, 125);
+        await plc.WriteByteAsync(1, 163, 126);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("6. 开始单点异步读 Byte");
+        Console.WriteLine(await plc.ReadByteAsync(1, 158));
+        Console.WriteLine(await plc.ReadByteAsync(1, 159));
+        Console.WriteLine(await plc.ReadByteAsync(1, 160));
+        Console.WriteLine(await plc.ReadByteAsync(1, 161));
+        Console.WriteLine(await plc.ReadByteAsync(1, 162));
+        Console.WriteLine(await plc.ReadByteAsync(1, 163));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("7. 开始连续异步写 Byte");
+        await plc.WriteByteAsync(1, 158, [11, 12, 13, 14, 15, 16]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("8. 开始连续异步读 Byte");
+        Console.WriteLine(string.Join(", ", await plc.ReadByteAsync(1, 158, 6)));
     }
 }
--- MyETool.S7NetPlusExample/Util/S7Util.Byte.cs
+++ MyETool.S7NetPlusExample/Util/S7Util.Byte.cs
@@ -4,33 +4,75 @@ namespace MyETool.S7NetPlusExample.Util;
 
 public static partial class S7Util
 {
-    /// <summary>
-    /// 在 S7 中  Byte = 1B = 8b
-    /// 在 C# 中  byte = 1B = 8b
-    /// </summary>
-    private const int ByteBitLength = 8;
+    private const int ByteByteLength = 1;
 
+    /// <summary>
+    /// 【同步】单点读取 Byte
+    /// </summary>
     public static byte ReadByte(this Plc plc, int db, int byteAdr)
     {
-        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, ByteBitLength / 8);
-        byte value = S7.Net.Types.Byte.FromByteArray(bytes);
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, ByteByteLength);
+        byte value = bytes[0];
         return value;
     }
 
+    /// <summary>
+    /// 【同步】批量读取连续的 Byte
+    /// </summary>
+    public static byte[] ReadByte(this Plc plc, int db, int byteAdr, int count)
+    {
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, ByteByteLength * count);
+        return bytes;
+    }
+
+    /// <summary>
+    /// 【同步】单点写入 Byte
+    /// </summary>
     public static void WriteByte(this Plc plc, int db, int byteAdr, byte value)
     {
-        plc.WriteBytes(DataType.DataBlock, db, byteAdr, S7.Net.Types.Byte.ToByteArray(value));
+        plc.WriteBytes(DataType.DataBlock, db, byteAdr, [value]);
     }
 
+    /// <summary>
+    /// 【同步】批量写入连续的 Byte
+    /// </summary>
+    public static void WriteByte(this Plc plc, int db, int byteAdr, byte[] values)
+    {
+        plc.WriteBytes(DataType.DataBlock, db, byteAdr, values);
+    }
+
+    /// <summary>
+    /// 【异步】单点读取 Byte
+    /// </summary>
     public static async Task<byte> ReadByteAsync(this Plc plc, int db, int byteAdr, CancellationToken cancellationToken = default)
     {
-        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, ByteBitLength / 8, cancellationToken);
-        byte value = S7.Net.Types.Byte.FromByteArray(bytes);
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, ByteByteLength, cancellationToken);
+        byte value = bytes[0];
         return value;
     }
 
+    /// <summary>
+    /// 【异步】批量读取连续的 Byte
+    /// </summary>
+    public static async Task<byte[]> ReadByteAsync(this Plc plc, int db, int byteAdr, int count, CancellationToken cancellationToken = default)
+    {
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, ByteByteLength * count, cancellationToken);
+        return bytes;
+    }
+
+    /// <summary>
+    /// 【异步】单点写入 Byte
+    /// </summary>
     public static async Task WriteByteAsync(this Plc plc, int db, int byteAdr, byte value, CancellationToken cancellationToken = default)
     {
-        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, S7.Net.Types.Byte.ToByteArray(value), cancellationToken);
+        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, [value], cancellationToken);
+    }
+
+    /// <summary>
+    /// 【异步】批量写入连续的 Byte
+    /// </summary>
+    public static async Task WriteByteAsync(this Plc plc, int db, int byteAdr, byte[] values, CancellationToken cancellationToken = default)
+    {
+        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, values, cancellationToken);
     }
 }
