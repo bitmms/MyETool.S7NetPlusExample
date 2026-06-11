81f968eabaaeafee2c0c6eee51c4302a4d1826fc
bitm <1759743261@qq.com>
2026-06-11 10:07:31 +0800

    feat: 增加批量读写连续的 Char 类型数值的方法以及相关测试

--- MyETool.S7NetPlusExample/Test/CharTest.cs
+++ MyETool.S7NetPlusExample/Test/CharTest.cs
@@ -7,32 +7,62 @@ public static class CharTest
 {
     public static async Task Test(Plc plc)
     {
-        Console.WriteLine("1. 开始同步写 ==========");
-        plc.WriteChar(1, 466, '+');
-        plc.WriteChar(1, 467, '-');
-        plc.WriteChar(1, 468, '*');
-
-        Console.WriteLine("2. 开始同步读 ==========");
-        char c1 = plc.ReadChar(1, 466);
-        char c2 = plc.ReadChar(1, 467);
-        char c3 = plc.ReadChar(1, 468);
-        Console.WriteLine(c1);
-        Console.WriteLine(c2);
-        Console.WriteLine(c3);
+        Console.WriteLine("1. 开始单点同步写 Char");
+        plc.WriteChar(1, 164, 'a');
+        plc.WriteChar(1, 165, 'b');
+        plc.WriteChar(1, 166, '中');
+        plc.WriteChar(1, 167, '国');
+        plc.WriteChar(1, 168, '1');
+        plc.WriteChar(1, 169, '2');
 
         await Task.Delay(1000);
 
-        Console.WriteLine("3. 开始异步写 ==========");
-        await plc.WriteCharAsync(1, 466, 'a');
-        await plc.WriteCharAsync(1, 467, 'b');
-        await plc.WriteCharAsync(1, 468, 'c');
+        Console.WriteLine("2. 开始单点同步读 Char");
+        Console.WriteLine(plc.ReadChar(1, 164));
+        Console.WriteLine(plc.ReadChar(1, 165));
+        Console.WriteLine(plc.ReadChar(1, 166));
+        Console.WriteLine(plc.ReadChar(1, 167));
+        Console.WriteLine(plc.ReadChar(1, 168));
+        Console.WriteLine(plc.ReadChar(1, 169));
 
-        Console.WriteLine("4. 开始异步读 ==========");
-        char c4 = await plc.ReadCharAsync(1, 466);
-        char c5 = await plc.ReadCharAsync(1, 467);
-        char c6 = await plc.ReadCharAsync(1, 468);
-        Console.WriteLine(c4);
-        Console.WriteLine(c5);
-        Console.WriteLine(c6);
+        await Task.Delay(1000);
+
+        Console.WriteLine("3. 开始连续同步写 Char");
+        plc.WriteChar(1, 164, ['c', 'd', '你', '好', '3', '4']);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("4. 开始连续同步读 Char");
+        Console.WriteLine(string.Join(", ", plc.ReadChar(1, 164, 6)));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("5. 开始单点异步写 Char");
+        await plc.WriteCharAsync(1, 164, 'a');
+        await plc.WriteCharAsync(1, 165, 'b');
+        await plc.WriteCharAsync(1, 166, '中');
+        await plc.WriteCharAsync(1, 167, '国');
+        await plc.WriteCharAsync(1, 168, '1');
+        await plc.WriteCharAsync(1, 169, '2');
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("6. 开始单点异步读 Char");
+        Console.WriteLine(await plc.ReadCharAsync(1, 164));
+        Console.WriteLine(await plc.ReadCharAsync(1, 165));
+        Console.WriteLine(await plc.ReadCharAsync(1, 166));
+        Console.WriteLine(await plc.ReadCharAsync(1, 167));
+        Console.WriteLine(await plc.ReadCharAsync(1, 168));
+        Console.WriteLine(await plc.ReadCharAsync(1, 169));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("7. 开始连续异步写 Char");
+        await plc.WriteCharAsync(1, 164, ['c', 'd', '你', '好', '3', '4']);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("8. 开始连续异步读 Char");
+        Console.WriteLine(string.Join(", ", await plc.ReadCharAsync(1, 164, 6)));
     }
 }
--- MyETool.S7NetPlusExample/Util/S7Util.Char.cs
+++ MyETool.S7NetPlusExample/Util/S7Util.Char.cs
@@ -1,36 +1,105 @@
-﻿using S7.Net;
+﻿using System.Text;
+using S7.Net;
 
 namespace MyETool.S7NetPlusExample.Util;
 
 public static partial class S7Util
 {
-    /// <summary>
-    /// 在 S7 中  Char = 1B = 8b
-    /// 在 C# 中  char = 1B = 8b
-    /// </summary>
-    private const int CharBitLength = 8;
+    private const int CharByteLength = 1;
 
+    /// <summary>
+    /// 【同步】单点读取 Char
+    /// </summary>
     public static char ReadChar(this Plc plc, int db, int byteAdr)
     {
-        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, CharBitLength / 8);
-        char value = (char)S7.Net.Types.Byte.FromByteArray(bytes);
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, CharByteLength);
+        char value = (char)bytes[0];
         return value;
     }
 
+    /// <summary>
+    /// 【同步】批量读取连续的 Char
+    /// </summary>
+    public static char[] ReadChar(this Plc plc, int db, int byteAdr, int count)
+    {
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, CharByteLength * count);
+        char[] values = new char[bytes.Length];
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            values[i] = (char)bytes[i];
+        }
+
+        return values;
+    }
+
+    /// <summary>
+    /// 【同步】单点写入 Char
+    /// </summary>
     public static void WriteChar(this Plc plc, int db, int byteAdr, char value)
     {
-        plc.WriteBytes(DataType.DataBlock, db, byteAdr, S7.Net.Types.Byte.ToByteArray((byte)value));
+        plc.WriteBytes(DataType.DataBlock, db, byteAdr, [(byte)value]);
     }
 
+    /// <summary>
+    /// 【同步】批量写入连续的 Char
+    /// </summary>
+    public static void WriteChar(this Plc plc, int db, int byteAdr, char[] values)
+    {
+        byte[] bytes = new byte[values.Length];
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            bytes[i] = Encoding.BigEndianUnicode.GetBytes(values[i].ToString())[1];
+        }
+
+        plc.WriteBytes(DataType.DataBlock, db, byteAdr, bytes);
+    }
+
+    /// <summary>
+    /// 【异步】单点读取 Char
+    /// </summary>
     public static async Task<char> ReadCharAsync(this Plc plc, int db, int byteAdr, CancellationToken cancellationToken = default)
     {
-        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, CharBitLength / 8, cancellationToken);
-        char value = (char)S7.Net.Types.Byte.FromByteArray(bytes);
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, CharByteLength, cancellationToken);
+        char value = (char)bytes[0];
         return value;
     }
 
+    /// <summary>
+    /// 【异步】批量读取连续的 Char
+    /// </summary>
+    public static async Task<char[]> ReadCharAsync(this Plc plc, int db, int byteAdr, int count, CancellationToken cancellationToken = default)
+    {
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, CharByteLength * count, cancellationToken);
+        char[] values = new char[bytes.Length];
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            values[i] = (char)bytes[i];
+        }
+
+        return values;
+    }
+
+    /// <summary>
+    /// 【异步】单点写入 Char
+    /// </summary>
     public static async Task WriteCharAsync(this Plc plc, int db, int byteAdr, char value, CancellationToken cancellationToken = default)
     {
-        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, S7.Net.Types.Byte.ToByteArray((byte)value), cancellationToken);
+        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, [(byte)value], cancellationToken);
+    }
+
+    /// <summary>
+    /// 【异步】批量写入连续的 Char
+    /// </summary>
+    public static async Task WriteCharAsync(this Plc plc, int db, int byteAdr, char[] values, CancellationToken cancellationToken = default)
+    {
+        byte[] bytes = new byte[values.Length];
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            bytes[i] = Encoding.BigEndianUnicode.GetBytes(values[i].ToString())[1];
+        }
+
+        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, bytes, cancellationToken);
     }
 }
