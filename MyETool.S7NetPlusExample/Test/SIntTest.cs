5f3cf7ba5e9445b073c2cb170dbe6fbcedb4b5a8
bitm <1759743261@qq.com>
2026-06-11 09:13:00 +0800

    feat: 增加批量读写连续的 SInt 类型数值的方法以及相关测试

--- MyETool.S7NetPlusExample/Test/SIntTest.cs
+++ MyETool.S7NetPlusExample/Test/SIntTest.cs
@@ -7,32 +7,62 @@ public static class SIntTest
 {
     public static async Task Test(Plc plc)
     {
-        Console.WriteLine("1. 开始同步写 ==========");
-        plc.WriteSInt(1, 2173, -31);
-        plc.WriteSInt(1, 2174, 32);
-        plc.WriteSInt(1, 2175, -33);
-
-        Console.WriteLine("2. 开始同步读 ==========");
-        sbyte sint1 = plc.ReadSInt(1, 2173);
-        sbyte sint2 = plc.ReadSInt(1, 2174);
-        sbyte sint3 = plc.ReadSInt(1, 2175);
-        Console.WriteLine(sint1);
-        Console.WriteLine(sint2);
-        Console.WriteLine(sint3);
+        Console.WriteLine("1. 开始单点同步写 SInt");
+        plc.WriteSInt(1, 152, -111);
+        plc.WriteSInt(1, 153, -112);
+        plc.WriteSInt(1, 154, -113);
+        plc.WriteSInt(1, 155, -114);
+        plc.WriteSInt(1, 156, -115);
+        plc.WriteSInt(1, 157, -116);
 
         await Task.Delay(1000);
 
-        Console.WriteLine("3. 开始异步写 ==========");
-        await plc.WriteSIntAsync(1, 2173, 61);
-        await plc.WriteSIntAsync(1, 2174, -62);
-        await plc.WriteSIntAsync(1, 2175, 63);
+        Console.WriteLine("2. 开始单点同步读 SInt");
+        Console.WriteLine(plc.ReadSInt(1, 152));
+        Console.WriteLine(plc.ReadSInt(1, 153));
+        Console.WriteLine(plc.ReadSInt(1, 154));
+        Console.WriteLine(plc.ReadSInt(1, 155));
+        Console.WriteLine(plc.ReadSInt(1, 156));
+        Console.WriteLine(plc.ReadSInt(1, 157));
 
-        Console.WriteLine("4. 开始异步读 ==========");
-        sbyte sint4 = await plc.ReadSIntAsync(1, 2173);
-        sbyte sint5 = await plc.ReadSIntAsync(1, 2174);
-        sbyte sint6 = await plc.ReadSIntAsync(1, 2175);
-        Console.WriteLine(sint4);
-        Console.WriteLine(sint5);
-        Console.WriteLine(sint6);
+        await Task.Delay(1000);
+
+        Console.WriteLine("3. 开始连续同步写 SInt");
+        plc.WriteSInt(1, 152, [-121, -122, -123, -124, -125, -126]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("4. 开始连续同步读 SInt");
+        Console.WriteLine(string.Join(", ", plc.ReadSInt(1, 152, 6)));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("5. 开始单点异步写 SInt");
+        await plc.WriteSIntAsync(1, 152, -101);
+        await plc.WriteSIntAsync(1, 153, -102);
+        await plc.WriteSIntAsync(1, 154, -103);
+        await plc.WriteSIntAsync(1, 155, -104);
+        await plc.WriteSIntAsync(1, 156, -105);
+        await plc.WriteSIntAsync(1, 157, -106);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("6. 开始单点异步读 SInt");
+        Console.WriteLine(await plc.ReadSIntAsync(1, 152));
+        Console.WriteLine(await plc.ReadSIntAsync(1, 153));
+        Console.WriteLine(await plc.ReadSIntAsync(1, 154));
+        Console.WriteLine(await plc.ReadSIntAsync(1, 155));
+        Console.WriteLine(await plc.ReadSIntAsync(1, 156));
+        Console.WriteLine(await plc.ReadSIntAsync(1, 157));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("7. 开始连续异步写 SInt");
+        await plc.WriteSIntAsync(1, 152, [61, 62, 63, 64, 65, 66]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("8. 开始连续异步读 SInt");
+        Console.WriteLine(string.Join(", ", await plc.ReadSIntAsync(1, 152, 6)));
     }
 }
--- MyETool.S7NetPlusExample/Util/S7Util.SInt.cs
+++ MyETool.S7NetPlusExample/Util/S7Util.SInt.cs
@@ -4,37 +4,130 @@ namespace MyETool.S7NetPlusExample.Util
 {
     public static partial class S7Util
     {
-        /// <summary>
-        /// 在 S7 中  SInt = 1B = 8b，[-128,127]
-        /// 在 C# 中  sbyte = 1B = 8b，[-128,127]
-        /// </summary>
-        private const int SIntBitLength = 8;
+        private const int SIntByteLength = 1;
 
+        /// <summary>
+        /// 【同步】单点读取 SInt
+        /// </summary>
         public static sbyte ReadSInt(this Plc plc, int db, int byteAdr)
         {
-            byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, SIntBitLength / 8);
+            byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, SIntByteLength);
             sbyte value = (sbyte)bytes[0];
             return value;
         }
 
+        /// <summary>
+        /// 【同步】批量读取连续的 SInt
+        /// </summary>
+        public static sbyte[] ReadSInt(this Plc plc, int db, int byteAdr, int count)
+        {
+            byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, SIntByteLength * count);
+            sbyte[] value = bytes.ToSByteArray();
+            return value;
+        }
+
+        /// <summary>
+        /// 【同步】单点写入 SInt
+        /// </summary>
         public static void WriteSInt(this Plc plc, int db, int byteAdr, sbyte value)
         {
-            byte[] bytes = [(byte)(value & 0xFF)];
+            byte[] bytes = [value.ToByte()];
             plc.WriteBytes(DataType.DataBlock, db, byteAdr, bytes);
         }
 
+        /// <summary>
+        /// 【同步】批量写入连续的 SInt
+        /// </summary>
+        public static void WriteSInt(this Plc plc, int db, int byteAdr, sbyte[] values)
+        {
+            byte[] bytes = values.ToByteArray();
+            plc.WriteBytes(DataType.DataBlock, db, byteAdr, bytes);
+        }
+
+        /// <summary>
+        /// 【异步】单点读取 SInt
+        /// </summary>
         public static async Task<sbyte> ReadSIntAsync(this Plc plc, int db, int byteAdr, CancellationToken cancellationToken = default)
         {
-            byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, SIntBitLength / 8, cancellationToken);
-            sbyte value = (sbyte)bytes[0];
+            byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, SIntByteLength, cancellationToken);
+            sbyte value = bytes[0].ToSByte();
             return value;
         }
 
+        /// <summary>
+        /// 【异步】批量读取连续的 SInt
+        /// </summary>
+        public static async Task<sbyte[]> ReadSIntAsync(this Plc plc, int db, int byteAdr, int count, CancellationToken cancellationToken = default)
+        {
+            byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, SIntByteLength * count, cancellationToken);
+            sbyte[] value = bytes.ToSByteArray();
+            return value;
+        }
+
+        /// <summary>
+        /// 【异步】单点写入 SInt
+        /// </summary>
         public static async Task WriteSIntAsync(this Plc plc, int db, int byteAdr, sbyte value, CancellationToken cancellationToken = default)
         {
-            // sbyte → byte 正确转换
-            byte[] bytes = [(byte)(value & 0xFF)];
+            byte[] bytes = [value.ToByte()];
             await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, bytes, cancellationToken);
+        }
+
+        /// <summary>
+        /// 【异步】批量写入连续的 SInt
+        /// </summary>
+        public static async Task WriteSIntAsync(this Plc plc, int db, int byteAdr, sbyte[] values, CancellationToken cancellationToken = default)
+        {
+            byte[] bytes = values.ToByteArray();
+            await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, bytes, cancellationToken);
+        }
+
+        /// <summary>
+        /// sbyte 转 byte
+        /// </summary>
+        private static byte ToByte(this sbyte value)
+        {
+            return (byte)(value & 0xFF);
+        }
+
+        /// <summary>
+        /// byte 转 sbyte
+        /// </summary>
+        private static sbyte ToSByte(this byte value)
+        {
+            return (sbyte)value;
+        }
+
+        /// <summary>
+        /// sbyte[] 转 byte[]
+        /// </summary>
+        private static byte[] ToByteArray(this sbyte[] values)
+        {
+            int len = values.Length;
+
+            byte[] bytes = new byte[len];
+            for (int i = 0; i < len; i++)
+            {
+                bytes[i] = (byte)(values[i] & 0xFF);
+            }
+
+            return bytes;
+        }
+
+        /// <summary>
+        /// byte[] 转 sbyte[]
+        /// </summary>
+        private static sbyte[] ToSByteArray(this byte[] values)
+        {
+            int len = values.Length;
+
+            sbyte[] bytes = new sbyte[len];
+            for (int i = 0; i < len; i++)
+            {
+                bytes[i] = (sbyte)(values[i]);
+            }
+
+            return bytes;
         }
     }
 }
