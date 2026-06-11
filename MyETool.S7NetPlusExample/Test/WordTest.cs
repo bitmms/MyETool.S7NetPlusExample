56998d1af81985f29ce79ad93c230789ec9efa11
bitm <1759743261@qq.com>
2026-06-11 11:22:38 +0800

    feat: 增加批量读写连续的 Word 类型数值的方法以及相关测试

--- MyETool.S7NetPlusExample/Test/WordTest.cs
+++ MyETool.S7NetPlusExample/Test/WordTest.cs
@@ -7,32 +7,62 @@ public static class WordTest
 {
     public static async Task Test(Plc plc)
     {
-        Console.WriteLine("1. 开始同步写 ==========");
-        plc.WriteWord(1, 18, 1001);
-        plc.WriteWord(1, 20, 1002);
-        plc.WriteWord(1, 22, 1003);
-
-        Console.WriteLine("2. 开始同步读 ==========");
-        ushort ushort1 = plc.ReadWord(1, 18);
-        ushort ushort2 = plc.ReadWord(1, 20);
-        ushort ushort3 = plc.ReadWord(1, 22);
-        Console.WriteLine(ushort1);
-        Console.WriteLine(ushort2);
-        Console.WriteLine(ushort3);
+        Console.WriteLine("1. 开始单点同步写 Word");
+        plc.WriteWord(1, 182, 211);
+        plc.WriteWord(1, 184, 212);
+        plc.WriteWord(1, 186, 213);
+        plc.WriteWord(1, 188, 214);
+        plc.WriteWord(1, 190, 215);
+        plc.WriteWord(1, 192, 216);
 
         await Task.Delay(1000);
 
-        Console.WriteLine("3. 开始异步写 ==========");
-        await plc.WriteWordAsync(1, 18, 101);
-        await plc.WriteWordAsync(1, 20, 102);
-        await plc.WriteWordAsync(1, 22, 103);
+        Console.WriteLine("2. 开始单点同步读 Word");
+        Console.WriteLine(plc.ReadWord(1, 182));
+        Console.WriteLine(plc.ReadWord(1, 184));
+        Console.WriteLine(plc.ReadWord(1, 186));
+        Console.WriteLine(plc.ReadWord(1, 188));
+        Console.WriteLine(plc.ReadWord(1, 190));
+        Console.WriteLine(plc.ReadWord(1, 192));
 
-        Console.WriteLine("4. 开始异步读 ==========");
-        ushort ushort4 = await plc.ReadWordAsync(1, 18);
-        ushort ushort5 = await plc.ReadWordAsync(1, 20);
-        ushort ushort6 = await plc.ReadWordAsync(1, 22);
-        Console.WriteLine(ushort4);
-        Console.WriteLine(ushort5);
-        Console.WriteLine(ushort6);
+        await Task.Delay(1000);
+
+        Console.WriteLine("3. 开始连续同步写 Word");
+        plc.WriteWord(1, 182, [221, 222, 223, 224, 225, 226]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("4. 开始连续同步读 Word");
+        Console.WriteLine(string.Join(", ", plc.ReadWord(1, 182, 6)));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("5. 开始单点异步写 Word");
+        await plc.WriteWordAsync(1, 182, 231);
+        await plc.WriteWordAsync(1, 184, 232);
+        await plc.WriteWordAsync(1, 186, 233);
+        await plc.WriteWordAsync(1, 188, 234);
+        await plc.WriteWordAsync(1, 190, 235);
+        await plc.WriteWordAsync(1, 192, 236);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("6. 开始单点异步读 Word");
+        Console.WriteLine(await plc.ReadWordAsync(1, 182));
+        Console.WriteLine(await plc.ReadWordAsync(1, 184));
+        Console.WriteLine(await plc.ReadWordAsync(1, 186));
+        Console.WriteLine(await plc.ReadWordAsync(1, 188));
+        Console.WriteLine(await plc.ReadWordAsync(1, 190));
+        Console.WriteLine(await plc.ReadWordAsync(1, 192));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("7. 开始连续异步写 Word");
+        await plc.WriteWordAsync(1, 182, [241, 242, 243, 244, 245, 246]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("8. 开始连续异步读 Word");
+        Console.WriteLine(string.Join(", ", await plc.ReadWordAsync(1, 182, 6)));
     }
 }
--- MyETool.S7NetPlusExample/Util/S7Util.Word.cs
+++ MyETool.S7NetPlusExample/Util/S7Util.Word.cs
@@ -5,24 +5,30 @@ namespace MyETool.S7NetPlusExample.Util;
 
 public static partial class S7Util
 {
-    /// <summary>
-    /// 在 S7 中  Word = 2B = 16b，表示一个 16 进制的数值
-    /// 在 C# 中  UInt16 = ushort = 2B = 16b
-    /// </summary>
-    private const int WordBitLength = 16;
+    private const int WordByteLength = 2;
 
     /// <summary>
-    /// 同步读取一个 Word
+    /// 【同步】单点读取 Word
     /// </summary>
     public static ushort ReadWord(this Plc plc, int db, int byteAdr)
     {
-        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, WordBitLength / 8);
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, WordByteLength);
         ushort value = Word.FromByteArray(bytes);
         return value;
     }
 
     /// <summary>
-    /// 同步写入一个 Word
+    /// 【同步】批量读取连续的 Word
+    /// </summary>
+    public static ushort[] ReadWord(this Plc plc, int db, int byteAdr, int count)
+    {
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, WordByteLength * count);
+        ushort[] values = Word.ToArray(bytes);
+        return values;
+    }
+
+    /// <summary>
+    /// 【同步】单点写入 Word
     /// </summary>
     public static void WriteWord(this Plc plc, int db, int byteAdr, ushort value)
     {
@@ -30,20 +36,46 @@ public static partial class S7Util
     }
 
     /// <summary>
-    /// 异步读取一个 Word
+    /// 【同步】批量写入连续的 Word
+    /// </summary>
+    public static void WriteWord(this Plc plc, int db, int byteAdr, ushort[] values)
+    {
+        plc.WriteBytes(DataType.DataBlock, db, byteAdr, Word.ToByteArray(values));
+    }
+
+    /// <summary>
+    /// 【异步】单点读取 Word
     /// </summary>
     public static async Task<ushort> ReadWordAsync(this Plc plc, int db, int byteAdr, CancellationToken cancellationToken = default)
     {
-        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, WordBitLength / 8, cancellationToken);
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, WordByteLength, cancellationToken);
         ushort value = Word.FromByteArray(bytes);
         return value;
     }
 
     /// <summary>
-    /// 异步写入一个 Word
+    /// 【异步】批量读取连续的 Word
+    /// </summary>
+    public static async Task<ushort[]> ReadWordAsync(this Plc plc, int db, int byteAdr, int count, CancellationToken cancellationToken = default)
+    {
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, WordByteLength * count, cancellationToken);
+        ushort[] values = Word.ToArray(bytes);
+        return values;
+    }
+
+    /// <summary>
+    /// 【异步】单点写入 Word
     /// </summary>
     public static async Task WriteWordAsync(this Plc plc, int db, int byteAdr, ushort value, CancellationToken cancellationToken = default)
     {
         await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, Word.ToByteArray(value), cancellationToken);
+    }
+
+    /// <summary>
+    /// 【异步】批量写入连续的 Word
+    /// </summary>
+    public static async Task WriteWordAsync(this Plc plc, int db, int byteAdr, ushort[] values, CancellationToken cancellationToken = default)
+    {
+        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, Word.ToByteArray(values), cancellationToken);
     }
 }
