a6118b4ef89a6ea16c33d2900a6fd4cf7a23fa86
bitm <1759743261@qq.com>
2026-06-11 08:24:24 +0800

    feat: 增加批量读写连续的 DInt 类型数值的方法以及相关测试

--- MyETool.S7NetPlusExample/Test/DIntTest.cs
+++ MyETool.S7NetPlusExample/Test/DIntTest.cs
@@ -7,32 +7,62 @@ public static class DIntTest
 {
     public static async Task Test(Plc plc)
     {
-        Console.WriteLine("1. 开始同步写 ==========");
-        plc.WriteDInt(1, 4, 1001);
-        plc.WriteDInt(1, 8, 1002);
-        plc.WriteDInt(1, 12, 1003);
-
-        Console.WriteLine("2. 开始同步读 ==========");
-        int i1 = plc.ReadDInt(1, 4);
-        int i2 = plc.ReadDInt(1, 8);
-        int i3 = plc.ReadDInt(1, 12);
-        Console.WriteLine(i1);
-        Console.WriteLine(i2);
-        Console.WriteLine(i3);
+        Console.WriteLine("1. 开始单点同步写 DInt");
+        plc.WriteDInt(1, 98, 211);
+        plc.WriteDInt(1, 102, 212);
+        plc.WriteDInt(1, 106, 213);
+        plc.WriteDInt(1, 110, 214);
+        plc.WriteDInt(1, 114, 215);
+        plc.WriteDInt(1, 118, 216);
 
         await Task.Delay(1000);
 
-        Console.WriteLine("3. 开始异步写 ==========");
-        await plc.WriteDIntAsync(1, 4, 5001);
-        await plc.WriteDIntAsync(1, 8, 5002);
-        await plc.WriteDIntAsync(1, 12, 5003);
+        Console.WriteLine("2. 开始单点同步读 DInt");
+        Console.WriteLine(plc.ReadDInt(1, 98));
+        Console.WriteLine(plc.ReadDInt(1, 102));
+        Console.WriteLine(plc.ReadDInt(1, 106));
+        Console.WriteLine(plc.ReadDInt(1, 110));
+        Console.WriteLine(plc.ReadDInt(1, 114));
+        Console.WriteLine(plc.ReadDInt(1, 118));
 
-        Console.WriteLine("4. 开始异步读 ==========");
-        int i4 = await plc.ReadDIntAsync(1, 4);
-        int i5 = await plc.ReadDIntAsync(1, 8);
-        int i6 = await plc.ReadDIntAsync(1, 12);
-        Console.WriteLine(i4);
-        Console.WriteLine(i5);
-        Console.WriteLine(i6);
+        await Task.Delay(1000);
+
+        Console.WriteLine("3. 开始连续同步写 DInt");
+        plc.WriteDInt(1, 98, [221, 222, 223, 224, 225, 226]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("4. 开始连续同步读 DInt");
+        Console.WriteLine(string.Join(", ", plc.ReadDInt(1, 98, 6)));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("5. 开始单点异步写 DInt");
+        await plc.WriteDIntAsync(1, 98, 231);
+        await plc.WriteDIntAsync(1, 102, 232);
+        await plc.WriteDIntAsync(1, 106, 233);
+        await plc.WriteDIntAsync(1, 110, 234);
+        await plc.WriteDIntAsync(1, 114, 235);
+        await plc.WriteDIntAsync(1, 118, 236);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("6. 开始单点异步读 DInt");
+        Console.WriteLine(await plc.ReadDIntAsync(1, 98));
+        Console.WriteLine(await plc.ReadDIntAsync(1, 102));
+        Console.WriteLine(await plc.ReadDIntAsync(1, 106));
+        Console.WriteLine(await plc.ReadDIntAsync(1, 110));
+        Console.WriteLine(await plc.ReadDIntAsync(1, 114));
+        Console.WriteLine(await plc.ReadDIntAsync(1, 118));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("7. 开始连续异步写 DInt");
+        await plc.WriteDIntAsync(1, 98, [241, 242, 243, 244, 245, 246]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("8. 开始连续异步读 DInt");
+        Console.WriteLine(string.Join(", ", await plc.ReadDIntAsync(1, 98, 6)));
     }
 }
--- MyETool.S7NetPlusExample/Util/S7Util.DInt.cs
+++ MyETool.S7NetPlusExample/Util/S7Util.DInt.cs
@@ -5,33 +5,77 @@ namespace MyETool.S7NetPlusExample.Util;
 
 public static partial class S7Util
 {
-    /// <summary>
-    /// 在 S7 中  DInt = 4B = 32b
-    /// 在 C# 中  Int32 = int = 4B = 32b
-    /// </summary>
-    private const int DIntBitLength = 32;
+    private const int DIntByteLength = 4;
 
+    /// <summary>
+    /// 【同步】单点读取 DInt
+    /// </summary>
     public static int ReadDInt(this Plc plc, int db, int byteAdr)
     {
-        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, DIntBitLength / 8);
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, DIntByteLength);
         int value = DInt.FromByteArray(bytes);
         return value;
     }
 
+    /// <summary>
+    /// 【同步】批量读取连续的 DInt
+    /// </summary>
+    public static int[] ReadDInt(this Plc plc, int db, int byteAdr, int count)
+    {
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, DIntByteLength * count);
+        int[] values = DInt.ToArray(bytes);
+        return values;
+    }
+
+    /// <summary>
+    /// 【同步】单点写入 DInt
+    /// </summary>
     public static void WriteDInt(this Plc plc, int db, int byteAdr, int value)
     {
         plc.WriteBytes(DataType.DataBlock, db, byteAdr, DInt.ToByteArray(value));
     }
 
+    /// <summary>
+    /// 【同步】批量写入连续的 DInt
+    /// </summary>
+    public static void WriteDInt(this Plc plc, int db, int byteAdr, int[] values)
+    {
+        plc.WriteBytes(DataType.DataBlock, db, byteAdr, DInt.ToByteArray(values));
+    }
+
+    /// <summary>
+    /// 【异步】单点读取 DInt
+    /// </summary>
     public static async Task<int> ReadDIntAsync(this Plc plc, int db, int byteAdr, CancellationToken cancellationToken = default)
     {
-        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, DIntBitLength / 8, cancellationToken);
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, DIntByteLength, cancellationToken);
         int value = DInt.FromByteArray(bytes);
         return value;
     }
 
+    /// <summary>
+    /// 【异步】批量读取连续的 DInt
+    /// </summary>
+    public static async Task<int[]> ReadDIntAsync(this Plc plc, int db, int byteAdr, int count, CancellationToken cancellationToken = default)
+    {
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, DIntByteLength * count, cancellationToken);
+        int[] values = DInt.ToArray(bytes);
+        return values;
+    }
+
+    /// <summary>
+    /// 【异步】单点写入 DInt
+    /// </summary>
     public static async Task WriteDIntAsync(this Plc plc, int db, int byteAdr, int value, CancellationToken cancellationToken = default)
     {
         await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, DInt.ToByteArray(value), cancellationToken);
+    }
+
+    /// <summary>
+    /// 【异步】批量写入连续的 DInt
+    /// </summary>
+    public static async Task WriteDIntAsync(this Plc plc, int db, int byteAdr, int[] values, CancellationToken cancellationToken = default)
+    {
+        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, DInt.ToByteArray(values), cancellationToken);
     }
 }
