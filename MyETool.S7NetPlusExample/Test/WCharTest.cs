58b3a9e7d3a4f5fca5b01fc1d3830b14c79c3bc1
bitm <1759743261@qq.com>
2026-06-11 10:38:46 +0800

    feat: 增加批量读写连续的 WChar 类型数值的方法以及相关测试

--- MyETool.S7NetPlusExample/Test/WCharTest.cs
+++ MyETool.S7NetPlusExample/Test/WCharTest.cs
@@ -7,22 +7,62 @@ public static class WCharTest
 {
     public static async Task Test(Plc plc)
     {
-        Console.WriteLine("1. 开始同步写 ==========");
-        plc.WriteWChar(1, 2166, '中');
-        plc.WriteWChar(1, 2168, '国');
-
-        Console.WriteLine("2. 开始同步读 ==========");
-        Console.WriteLine(plc.ReadWChar(1, 2166));
-        Console.WriteLine(plc.ReadWChar(1, 2168));
+        Console.WriteLine("1. 开始单点同步写 WChar");
+        plc.WriteWChar(1, 170, 'a');
+        plc.WriteWChar(1, 172, 'b');
+        plc.WriteWChar(1, 174, '中');
+        plc.WriteWChar(1, 176, '国');
+        plc.WriteWChar(1, 178, '1');
+        plc.WriteWChar(1, 180, '2');
 
         await Task.Delay(1000);
 
-        Console.WriteLine("3. 开始异步写 ==========");
-        await plc.WriteWCharAsync(1, 2166, '你');
-        await plc.WriteWCharAsync(1, 2168, '好');
+        Console.WriteLine("2. 开始单点同步读 WChar");
+        Console.WriteLine(plc.ReadWChar(1, 170));
+        Console.WriteLine(plc.ReadWChar(1, 172));
+        Console.WriteLine(plc.ReadWChar(1, 174));
+        Console.WriteLine(plc.ReadWChar(1, 176));
+        Console.WriteLine(plc.ReadWChar(1, 178));
+        Console.WriteLine(plc.ReadWChar(1, 180));
 
-        Console.WriteLine("4. 开始异步读 ==========");
-        Console.WriteLine(await plc.ReadWCharAsync(1, 2166));
-        Console.WriteLine(await plc.ReadWCharAsync(1, 2168));
+        await Task.Delay(1000);
+
+        Console.WriteLine("3. 开始连续同步写 WChar");
+        plc.WriteWChar(1, 170, ['c', 'd', '你', '好', '3', '4']);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("4. 开始连续同步读 WChar");
+        Console.WriteLine(string.Join(", ", plc.ReadWChar(1, 170, 6)));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("5. 开始单点异步写 WChar");
+        await plc.WriteWCharAsync(1, 170, 'a');
+        await plc.WriteWCharAsync(1, 172, 'b');
+        await plc.WriteWCharAsync(1, 174, '中');
+        await plc.WriteWCharAsync(1, 176, '国');
+        await plc.WriteWCharAsync(1, 178, '1');
+        await plc.WriteWCharAsync(1, 180, '2');
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("6. 开始单点异步读 WChar");
+        Console.WriteLine(await plc.ReadWCharAsync(1, 170));
+        Console.WriteLine(await plc.ReadWCharAsync(1, 172));
+        Console.WriteLine(await plc.ReadWCharAsync(1, 174));
+        Console.WriteLine(await plc.ReadWCharAsync(1, 176));
+        Console.WriteLine(await plc.ReadWCharAsync(1, 178));
+        Console.WriteLine(await plc.ReadWCharAsync(1, 180));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("7. 开始连续异步写 WChar");
+        await plc.WriteWCharAsync(1, 170, ['c', 'd', '你', '好', '3', '4']);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("8. 开始连续异步读 WChar");
+        Console.WriteLine(string.Join(", ", await plc.ReadWCharAsync(1, 170, 6)));
     }
 }
--- MyETool.S7NetPlusExample/Util/S7Util.WChar.cs
+++ MyETool.S7NetPlusExample/Util/S7Util.WChar.cs
@@ -5,39 +5,95 @@ namespace MyETool.S7NetPlusExample.Util;
 
 public static partial class S7Util
 {
-    /// <summary>
-    /// 在 S7 中  WChar = 2B = 16b，表示 Unicode UTF-16 大端 字符
-    /// 在 C# 中  char = 2B = 16b
-    /// </summary>
-    private const int WCharBitLength = 16;
+    private const int WCharByteLength = 2;
 
+    /// <summary>
+    /// 【同步】单点读取 WChar
+    /// </summary>
     public static char ReadWChar(this Plc plc, int db, int byteAdr)
     {
-        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, WCharBitLength / 2);
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, WCharByteLength);
         char resultChar = Encoding.BigEndianUnicode.GetChars(bytes)[0];
         return resultChar;
     }
 
+    /// <summary>
+    /// 【同步】批量读取连续的 WChar
+    /// </summary>
+    public static char[] ReadWChar(this Plc plc, int db, int byteAdr, int count)
+    {
+        // 1. 从 PLC 读取双字节字符，此时是 Unicode UTF-16 双字节大端
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, WCharByteLength * count);
+        // 2. 利用 Unicode 大端进行解码得到 char 数据
+        char[] resultChars = Encoding.BigEndianUnicode.GetChars(bytes);
+        // 3. 返回
+        return resultChars;
+    }
+
+    /// <summary>
+    /// 【同步】单点写入 WChar
+    /// </summary>
     public static void WriteWChar(this Plc plc, int db, int byteAdr, char value)
     {
         byte[] bytes = Encoding.BigEndianUnicode.GetBytes([value]);
         plc.WriteBytes(DataType.DataBlock, db, byteAdr, bytes);
     }
 
+    /// <summary>
+    /// 【同步】批量写入连续的 WChar
+    /// </summary>
+    public static void WriteWChar(this Plc plc, int db, int byteAdr, char[] values)
+    {
+        // 1. 利用 Unicode 大端进行解码，得到 Unicode UTF-16 大端双字节数组
+        byte[] bytes = Encoding.BigEndianUnicode.GetBytes(values);
+        // 2. 写入 PLC 中
+        plc.WriteBytes(DataType.DataBlock, db, byteAdr, bytes);
+    }
+
+    /// <summary>
+    /// 【异步】单点读取 WChar
+    /// </summary>
     public static async Task<char> ReadWCharAsync(this Plc plc, int db, int byteAdr, CancellationToken cancellationToken = default)
     {
         // 1. 从 PLC 读取双字节字符，此时是 Unicode UTF-16 双字节大端
-        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, WCharBitLength / 2, cancellationToken);
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, WCharByteLength, cancellationToken);
         // 2. 利用 Unicode 大端进行解码得到 char 数据
         char resultChar = Encoding.BigEndianUnicode.GetChars(bytes)[0];
         // 3. 返回
         return resultChar;
     }
 
+    /// <summary>
+    /// 【异步】批量读取连续的 WChar
+    /// </summary>
+    public static async Task<char[]> ReadWCharAsync(this Plc plc, int db, int byteAdr, int count, CancellationToken cancellationToken = default)
+    {
+        // 1. 从 PLC 读取双字节字符，此时是 Unicode UTF-16 双字节大端
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, WCharByteLength * count, cancellationToken);
+        // 2. 利用 Unicode 大端进行解码得到 char 数据
+        char[] resultChars = Encoding.BigEndianUnicode.GetChars(bytes);
+        // 3. 返回
+        return resultChars;
+    }
+
+    /// <summary>
+    /// 【异步】单点写入 WChar
+    /// </summary>
     public static async Task WriteWCharAsync(this Plc plc, int db, int byteAdr, char value, CancellationToken cancellationToken = default)
     {
         // 1. 利用 Unicode 大端进行解码，得到 Unicode UTF-16 大端双字节数组
         byte[] bytes = Encoding.BigEndianUnicode.GetBytes([value]);
+        // 2. 写入 PLC 中
+        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, bytes, cancellationToken);
+    }
+
+    /// <summary>
+    /// 【异步】批量写入连续的 WChar
+    /// </summary>
+    public static async Task WriteWCharAsync(this Plc plc, int db, int byteAdr, char[] values, CancellationToken cancellationToken = default)
+    {
+        // 1. 利用 Unicode 大端进行解码，得到 Unicode UTF-16 大端双字节数组
+        byte[] bytes = Encoding.BigEndianUnicode.GetBytes(values);
         // 2. 写入 PLC 中
         await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, bytes, cancellationToken);
     }
