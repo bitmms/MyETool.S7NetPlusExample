d989de45cfadf90c4b9de1814ccea363379536c5
bitm <1759743261@qq.com>
2026-06-11 14:52:51 +0800

    feat: 增加批量读写连续的 Time 类型数值的方法以及相关测试

--- MyETool.S7NetPlusExample/Test/TimeTest.cs
+++ MyETool.S7NetPlusExample/Test/TimeTest.cs
@@ -7,32 +7,77 @@ public static class TimeTest
 {
     public static async Task Test(Plc plc)
     {
-        Console.WriteLine("1. 开始同步写 ==========");
-        plc.WriteTime(1, 72, new TimeSpan(1, 2, 3, 4, 5));
-        plc.WriteTime(1, 76, new TimeSpan(1, 2, 3, 4, 5));
-        plc.WriteTime(1, 80, new TimeSpan(1, 2, 3, 4, 5));
-
-        Console.WriteLine("2. 开始同步读 ==========");
-        TimeSpan t1 = plc.ReadTime(1, 72);
-        TimeSpan t2 = plc.ReadTime(1, 76);
-        TimeSpan t3 = plc.ReadTime(1, 80);
-        Console.WriteLine(t1);
-        Console.WriteLine(t2);
-        Console.WriteLine(t3);
+        Console.WriteLine("1. 开始单点同步写 Time");
+        plc.WriteTime(1, 230, new TimeSpan(1, 2, 3, 4, 5));
+        plc.WriteTime(1, 234, new TimeSpan(3, 2, 3, 4, 5));
+        plc.WriteTime(1, 238, new TimeSpan(5, 2, 3, 4, 5));
+        plc.WriteTime(1, 242, new TimeSpan(7, 2, 3, 4, 5));
+        plc.WriteTime(1, 246, new TimeSpan(9, 2, 3, 4, 5));
+        plc.WriteTime(1, 250, new TimeSpan(0, 2, 3, 4, 5));
 
         await Task.Delay(1000);
 
-        Console.WriteLine("3. 开始异步写 ==========");
-        await plc.WriteTimeAsync(1, 72, new TimeSpan(2, 2, 3, 4, 5));
-        await plc.WriteTimeAsync(1, 76, new TimeSpan(2, 2, 3, 4, 5));
-        await plc.WriteTimeAsync(1, 80, new TimeSpan(2, 2, 3, 4, 5));
+        Console.WriteLine("2. 开始单点同步读 Time");
+        Console.WriteLine(plc.ReadTime(1, 230));
+        Console.WriteLine(plc.ReadTime(1, 234));
+        Console.WriteLine(plc.ReadTime(1, 238));
+        Console.WriteLine(plc.ReadTime(1, 242));
+        Console.WriteLine(plc.ReadTime(1, 246));
+        Console.WriteLine(plc.ReadTime(1, 250));
 
-        Console.WriteLine("4. 开始异步读 ==========");
-        TimeSpan t4 = await plc.ReadTimeAsync(1, 72);
-        TimeSpan t5 = await plc.ReadTimeAsync(1, 76);
-        TimeSpan t6 = await plc.ReadTimeAsync(1, 80);
-        Console.WriteLine(t4);
-        Console.WriteLine(t5);
-        Console.WriteLine(t6);
+        await Task.Delay(1000);
+
+        Console.WriteLine("3. 开始连续同步写 Time");
+        plc.WriteTime(1, 230, [
+            new TimeSpan(2, 1, 3, 4, 5),
+            new TimeSpan(2, 3, 3, 4, 5),
+            new TimeSpan(2, 5, 3, 4, 5),
+            new TimeSpan(2, 7, 3, 4, 5),
+            new TimeSpan(2, 9, 3, 4, 5),
+            new TimeSpan(2, 0, 3, 4, 5),
+        ]);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("4. 开始连续同步读 Time");
+        Console.WriteLine(string.Join(", ", plc.ReadTime(1, 230, 6)));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("5. 开始单点异步写 Time");
+        await plc.WriteTimeAsync(1, 230, new TimeSpan(3, 2, 3, 4, 5));
+        await plc.WriteTimeAsync(1, 234, new TimeSpan(3, 2, 3, 4, 5));
+        await plc.WriteTimeAsync(1, 238, new TimeSpan(3, 2, 3, 4, 5));
+        await plc.WriteTimeAsync(1, 242, new TimeSpan(3, 2, 3, 4, 5));
+        await plc.WriteTimeAsync(1, 246, new TimeSpan(3, 2, 3, 4, 5));
+        await plc.WriteTimeAsync(1, 250, new TimeSpan(3, 2, 3, 4, 5));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("6. 开始单点异步读 Time");
+        Console.WriteLine(await plc.ReadTimeAsync(1, 230));
+        Console.WriteLine(await plc.ReadTimeAsync(1, 234));
+        Console.WriteLine(await plc.ReadTimeAsync(1, 238));
+        Console.WriteLine(await plc.ReadTimeAsync(1, 242));
+        Console.WriteLine(await plc.ReadTimeAsync(1, 246));
+        Console.WriteLine(await plc.ReadTimeAsync(1, 250));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("7. 开始连续异步写 Time");
+        await plc.WriteTimeAsync(1, 230, [
+            new TimeSpan(10, 1, 3, 4, 5),
+            new TimeSpan(10, 3, 3, 4, 5),
+            new TimeSpan(10, 5, 3, 4, 5),
+            new TimeSpan(10, 7, 3, 4, 5),
+            new TimeSpan(10, 9, 3, 4, 5),
+            new TimeSpan(10, 0, 3, 4, 5),
+        ]);
+
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("8. 开始连续异步读 Time");
+        Console.WriteLine(string.Join(", ", await plc.ReadTimeAsync(1, 230, 6)));
     }
 }
--- MyETool.S7NetPlusExample/Util/S7Util.Time.cs
+++ MyETool.S7NetPlusExample/Util/S7Util.Time.cs
@@ -5,25 +5,38 @@ namespace MyETool.S7NetPlusExample.Util;
 
 public static partial class S7Util
 {
-    /// <summary>
-    /// 在 S7 中  Time = 4B = 32b
-    /// 在 C# 中  Int32 = int = 4B = 32b --->>> TimeSpan
-    /// </summary>
-    private const int TimeBitLength = 32;
+    private const int TimeByteLength = 4;
 
     /// <summary>
-    /// 同步读取一个 Time
+    /// 【同步】单点读取 Time
     /// </summary>
     public static TimeSpan ReadTime(this Plc plc, int db, int byteAdr)
     {
-        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, TimeBitLength / 8);
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, TimeByteLength);
         int value = DInt.FromByteArray(bytes);
         TimeSpan timeSpan = TimeSpan.FromMilliseconds(value);
         return timeSpan;
     }
 
     /// <summary>
-    /// 同步写入一个 Time
+    /// 【同步】批量读取连续的 Time
+    /// </summary>
+    public static TimeSpan[] ReadTime(this Plc plc, int db, int byteAdr, int count)
+    {
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, TimeByteLength * count);
+        int[] value = DInt.ToArray(bytes);
+
+        TimeSpan[] timeSpans = new TimeSpan[value.Length];
+        for (int i = 0; i < timeSpans.Length; i++)
+        {
+            timeSpans[i] = TimeSpan.FromMilliseconds(value[i]);
+        }
+
+        return timeSpans;
+    }
+
+    /// <summary>
+    /// 【同步】单点写入 Time
     /// </summary>
     public static void WriteTime(this Plc plc, int db, int byteAdr, TimeSpan value)
     {
@@ -41,18 +54,65 @@ public static partial class S7Util
     }
 
     /// <summary>
-    /// 异步读取一个 Time
+    /// 【同步】批量写入连续的 Time
+    /// </summary>
+    public static void WriteTime(this Plc plc, int db, int byteAdr, TimeSpan[] values)
+    {
+        byte[] bytes = new byte[values.Length * TimeByteLength];
+
+        for (int i = 0, j = 0; i < values.Length * 4 && j < values.Length; i += 4, j++)
+        {
+            TimeSpan item = values[j];
+            if (item < TimeSpan.FromMilliseconds(int.MinValue))
+            {
+                item = TimeSpan.FromMilliseconds(int.MinValue);
+            }
+
+            if (item > TimeSpan.FromMilliseconds(int.MaxValue))
+            {
+                item = TimeSpan.FromMilliseconds(int.MaxValue);
+            }
+
+            byte[] array = DInt.ToByteArray(Convert.ToInt32(item.TotalMilliseconds));
+            bytes[i] = array[0];
+            bytes[i + 1] = array[1];
+            bytes[i + 2] = array[2];
+            bytes[i + 3] = array[3];
+        }
+
+        plc.WriteBytes(DataType.DataBlock, db, byteAdr, bytes);
+    }
+
+    /// <summary>
+    /// 【异步】单点读取 Time
     /// </summary>
     public static async Task<TimeSpan> ReadTimeAsync(this Plc plc, int db, int byteAdr, CancellationToken cancellationToken = default)
     {
-        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, TimeBitLength / 8, cancellationToken);
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, TimeByteLength, cancellationToken);
         int value = DInt.FromByteArray(bytes);
         TimeSpan timeSpan = TimeSpan.FromMilliseconds(value);
         return timeSpan;
     }
 
     /// <summary>
-    /// 异步写入一个 Time
+    /// 【异步】批量读取连续的 Time
+    /// </summary>
+    public static async Task<TimeSpan[]> ReadTimeAsync(this Plc plc, int db, int byteAdr, int count, CancellationToken cancellationToken = default)
+    {
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, TimeByteLength * count, cancellationToken);
+        int[] value = DInt.ToArray(bytes);
+
+        TimeSpan[] timeSpans = new TimeSpan[value.Length];
+        for (int i = 0; i < timeSpans.Length; i++)
+        {
+            timeSpans[i] = TimeSpan.FromMilliseconds(value[i]);
+        }
+
+        return timeSpans;
+    }
+
+    /// <summary>
+    /// 【异步】单点写入 Time
     /// </summary>
     public static async Task WriteTimeAsync(this Plc plc, int db, int byteAdr, TimeSpan value, CancellationToken cancellationToken = default)
     {
@@ -67,5 +127,35 @@ public static partial class S7Util
         }
 
         await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, DInt.ToByteArray(Convert.ToInt32(value.TotalMilliseconds)), cancellationToken);
+    }
+
+    /// <summary>
+    /// 【异步】批量写入连续的 Time
+    /// </summary>
+    public static async Task WriteTimeAsync(this Plc plc, int db, int byteAdr, TimeSpan[] values, CancellationToken cancellationToken = default)
+    {
+        byte[] bytes = new byte[values.Length * TimeByteLength];
+
+        for (int i = 0, j = 0; i < values.Length * 4 && j < values.Length; i += 4, j++)
+        {
+            TimeSpan item = values[j];
+            if (item < TimeSpan.FromMilliseconds(int.MinValue))
+            {
+                item = TimeSpan.FromMilliseconds(int.MinValue);
+            }
+
+            if (item > TimeSpan.FromMilliseconds(int.MaxValue))
+            {
+                item = TimeSpan.FromMilliseconds(int.MaxValue);
+            }
+
+            byte[] array = DInt.ToByteArray(Convert.ToInt32(item.TotalMilliseconds));
+            bytes[i] = array[0];
+            bytes[i + 1] = array[1];
+            bytes[i + 2] = array[2];
+            bytes[i + 3] = array[3];
+        }
+
+        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, bytes, cancellationToken);
     }
 }
