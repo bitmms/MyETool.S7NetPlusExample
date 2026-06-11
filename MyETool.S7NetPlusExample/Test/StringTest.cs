577cdc68d7b8c4c77f2439185589677d2435ac1f
bitm <1759743261@qq.com>
2026-06-11 15:57:26 +0800

    feat: 增加批量读写连续的 String 类型数值的方法以及相关测试

--- MyETool.S7NetPlusExample/Test/StringTest.cs
+++ MyETool.S7NetPlusExample/Test/StringTest.cs
@@ -7,26 +7,76 @@ public static class StringTest
 {
     public static async Task Test(Plc plc)
     {
-        Console.WriteLine("1. 开始同步写 ==========");
-        plc.WriteString(1, 84, "Nothing is impossible...", 254);
-        plc.WriteString(1, 596, "abcdefg1234567", 254);
-        plc.WriteString(1, 852, "qwertyuiop", 10);
-
-        Console.WriteLine("2. 开始同步读 ==========");
-        Console.WriteLine(plc.ReadString(1, 84, 256));
-        Console.WriteLine(plc.ReadString(1, 596, 254));
-        Console.WriteLine(plc.ReadString(1, 852, 10));
+        Console.WriteLine("1. 开始单点同步写 String");
+        plc.WriteString(1, 254, "123456_hello_1", 254);
+        plc.WriteString(1, 510, "123456_hello_2", 254);
+        plc.WriteString(1, 766, "123456_hello_3", 254);
+        plc.WriteString(1, 1022, "123456_hello_4", 254);
+        plc.WriteString(1, 1278, "123456_hello_5", 254);
+        plc.WriteString(1, 1534, "123456_hello_6", 254);
 
         await Task.Delay(1000);
 
-        Console.WriteLine("3. 开始异步写 ==========");
-        await plc.WriteStringAsync(1, 84, "Goodbye", 254);
-        await plc.WriteStringAsync(1, 596, "You're welcome", 254);
-        await plc.WriteStringAsync(1, 852, "Every day is new", 10);
+        Console.WriteLine("2. 开始单点同步读 String");
+        Console.WriteLine(plc.ReadString(1, 254, 254));
+        Console.WriteLine(plc.ReadString(1, 510, 254));
+        Console.WriteLine(plc.ReadString(1, 766, 254));
+        Console.WriteLine(plc.ReadString(1, 1022, 254));
+        Console.WriteLine(plc.ReadString(1, 1278, 254));
+        Console.WriteLine(plc.ReadString(1, 1534, 254));
 
-        Console.WriteLine("4. 开始异步读 ==========");
-        Console.WriteLine(await plc.ReadStringAsync(1, 84, 256));
-        Console.WriteLine(await plc.ReadStringAsync(1, 596, 254));
-        Console.WriteLine(await plc.ReadStringAsync(1, 852, 10));
+        await Task.Delay(1000);
+
+        Console.WriteLine("3. 开始连续同步写 String");
+        plc.WriteString(1, 254, [
+            "hello_1",
+            "hello_2",
+            "hello_3",
+            "hello_4",
+            "hello_5",
+            "hello_6",
+        ], 254);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("4. 开始连续同步读 String");
+        Console.WriteLine(string.Join(", ", plc.ReadString(1, 254, 6, 254)));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("5. 开始单点异步写 String");
+        await plc.WriteStringAsync(1, 254, "abc111", 254);
+        await plc.WriteStringAsync(1, 510, "abc222", 254);
+        await plc.WriteStringAsync(1, 766, "abc333", 254);
+        await plc.WriteStringAsync(1, 1022, "abc444", 254);
+        await plc.WriteStringAsync(1, 1278, "abc555", 254);
+        await plc.WriteStringAsync(1, 1534, "abc666", 254);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("6. 开始单点异步读 String");
+        Console.WriteLine(await plc.ReadStringAsync(1, 254, 254));
+        Console.WriteLine(await plc.ReadStringAsync(1, 510, 254));
+        Console.WriteLine(await plc.ReadStringAsync(1, 766, 254));
+        Console.WriteLine(await plc.ReadStringAsync(1, 1022, 254));
+        Console.WriteLine(await plc.ReadStringAsync(1, 1278, 254));
+        Console.WriteLine(await plc.ReadStringAsync(1, 1534, 254));
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("7. 开始连续异步写 String");
+        await plc.WriteStringAsync(1, 254, [
+            "123456_hello_1",
+            "123456_hello_2",
+            "123456_hello_3",
+            "123456_hello_4",
+            "123456_hello_5",
+            "123456_hello_6",
+        ], 254);
+
+        await Task.Delay(1000);
+
+        Console.WriteLine("8. 开始连续异步读 String");
+        Console.WriteLine(string.Join(", ", await plc.ReadStringAsync(1, 254, 6, 254)));
     }
 }
--- MyETool.S7NetPlusExample/Util/S7Util.String.cs
+++ MyETool.S7NetPlusExample/Util/S7Util.String.cs
@@ -17,12 +17,15 @@ public static partial class S7Util
 
     private static readonly Encoding S7StringEncoding = Encoding.ASCII;
 
-    public static string ReadString(this Plc plc, int db, int byteAdr, int n)
+    /// <summary>
+    /// 【同步】单点读取 String
+    /// </summary>
+    public static string ReadString(this Plc plc, int db, int byteAdr, int maxCharCount)
     {
-        if (n <= 0) return "";
-        n = Math.Min(254, n);
+        if (maxCharCount <= 0) return "";
+        maxCharCount = Math.Min(254, maxCharCount);
 
-        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, 2 + n); // 这里保证了至少读取 2 个字节的数据
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, 2 + maxCharCount); // 这里保证了至少读取 2 个字节的数据
 
         int totalLength = bytes[0]; // 实际可写入字符串的字节数
         int contentLength = bytes[1]; // 实际写入字符串的字节数
@@ -32,25 +35,50 @@ public static partial class S7Util
         return fromByteArray;
     }
 
-    public static void WriteString(this Plc plc, int db, int byteAdr, string value, byte n)
+    /// <summary>
+    /// 【同步】批量读取连续的 String
+    /// </summary>
+    /// <returns></returns>
+    public static string[] ReadString(this Plc plc, int db, int byteAdr, int count, byte maxCharCount)
+    {
+        if (maxCharCount <= 0) return [];
+        maxCharCount = Math.Min((byte)254, maxCharCount);
+
+        byte[] bytes = plc.ReadBytes(DataType.DataBlock, db, byteAdr, (2 + maxCharCount) * count); // 这里保证了至少读取 2 个字节的数据
+
+        return S7ByteArrayToStringArray(bytes, maxCharCount);
+    }
+
+    /// <summary>
+    /// 【同步】单点写入 String
+    /// </summary>
+    public static void WriteString(this Plc plc, int db, int byteAdr, string value, byte maxCharCount)
     {
         byte[] bytes = S7StringEncoding.GetBytes(value);
-        n = Math.Min(n, (byte)254);
-        n = Math.Max(n, (byte)0);
-        byte[] destArray = new byte[2 + n];
-        destArray[0] = n;
-        destArray[1] = Math.Min((byte)bytes.Length, n);
+        maxCharCount = Math.Min(maxCharCount, (byte)254);
+        maxCharCount = Math.Max(maxCharCount, (byte)0);
+        byte[] destArray = new byte[2 + maxCharCount];
+        destArray[0] = maxCharCount;
+        destArray[1] = Math.Min((byte)bytes.Length, maxCharCount);
         Array.Copy(bytes, 0, destArray, 2, destArray[1]);
         plc.WriteBytes(DataType.DataBlock, db, byteAdr, destArray);
     }
 
     /// <summary>
-    /// 异步单点读取 S7-1200 PLC string 类型的数据【最多读取 254 个字节数据，溢出自动截断】
+    /// 【同步】批量写入连续的 String
+    /// </summary>
+    public static void WriteString(this Plc plc, int db, int byteAdr, string[] values, byte maxCharCount)
+    {
+        plc.WriteBytes(DataType.DataBlock, db, byteAdr, S7StringArrayToByteArray(values, maxCharCount));
+    }
+
+    /// <summary>
+    /// 【异步】单点读取 String
     /// </summary>
     /// <param name="plc">plc实例</param>
     /// <param name="db">DB号</param>
     /// <param name="byteAdr">起始地址</param>
-    /// <param name="maxCharCount">声明 string 时，string[n]，这里的 n 是实际可写入的字符数量</param>
+    /// <param name="maxCharCount">声明 string 时，string[n]，这里的 n 是实际可写入的字符数量，最多可写入 254 个字节数据，溢出自动截断</param>
     /// <param name="cancellationToken">取消异步的 token</param>
     /// <returns></returns>
     public static async Task<string> ReadStringAsync(this Plc plc, int db, int byteAdr, int maxCharCount, CancellationToken cancellationToken = default)
@@ -69,13 +97,27 @@ public static partial class S7Util
     }
 
     /// <summary>
-    /// 异步单点写入 S7-1200 PLC string 类型的数据【最多写入 n 个字节数据，溢出自动截断】
+    /// 【异步】批量读取连续的 String
+    /// </summary>
+    /// <returns></returns>
+    public static async Task<string[]> ReadStringAsync(this Plc plc, int db, int byteAdr, int count, byte maxCharCount, CancellationToken cancellationToken = default)
+    {
+        if (maxCharCount <= 0) return [];
+        maxCharCount = Math.Min((byte)254, maxCharCount);
+
+        byte[] bytes = await plc.ReadBytesAsync(DataType.DataBlock, db, byteAdr, (2 + maxCharCount) * count, cancellationToken); // 这里保证了至少读取 2 个字节的数据
+
+        return S7ByteArrayToStringArray(bytes, maxCharCount);
+    }
+
+    /// <summary>
+    /// 【异步】单点写入 String
     /// </summary>
     /// <param name="plc">plc实例</param>
     /// <param name="db">DB号</param>
     /// <param name="byteAdr">起始地址</param>
     /// <param name="value">写入的字符串</param>
-    /// <param name="maxCharCount">声明 string 时，string[n]，这里的 n 是实际可写入的字符数量</param>
+    /// <param name="maxCharCount">声明 string 时，string[n]，这里的 n 是实际可写入的字符数量，最多可写入 254 个字节数据，溢出自动截断</param>
     /// <param name="cancellationToken">取消异步的 token</param>
     /// <returns></returns>
     public static async Task WriteStringAsync(this Plc plc, int db, int byteAdr, string value, byte maxCharCount, CancellationToken cancellationToken = default)
@@ -94,5 +136,54 @@ public static partial class S7Util
         Array.Copy(bytes, 0, destArray, 2, destArray[1]);
         // 6. 写入 plc
         await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, destArray, cancellationToken);
+    }
+
+    /// <summary>
+    /// 【异步】批量写入连续的 String
+    /// </summary>
+    public static async Task WriteStringAsync(this Plc plc, int db, int byteAdr, string[] values, byte maxCharCount, CancellationToken cancellationToken = default)
+    {
+        await plc.WriteBytesAsync(DataType.DataBlock, db, byteAdr, S7StringArrayToByteArray(values, maxCharCount), cancellationToken);
+    }
+
+    private static byte[] S7StringArrayToByteArray(string[] stringArray, byte maxCharCount)
+    {
+        // 限制最大字符数 0~254
+        maxCharCount = Math.Min(maxCharCount, (byte)254);
+        maxCharCount = Math.Max(maxCharCount, (byte)0);
+
+        int myIdx = 0;
+        byte[] results = new byte[stringArray.Length * (2 + maxCharCount)];
+
+        foreach (var stringItem in stringArray)
+        {
+            byte[] contentBytes = S7StringEncoding.GetBytes(stringItem);
+            // 最大长度字节
+            results[myIdx++] = maxCharCount;
+            // 实际长度：严格限制在 0 ~ maxCharCount，避免byte溢出
+            byte actualLen = Math.Min((byte)contentBytes.Length, maxCharCount);
+            results[myIdx++] = actualLen;
+            // 内容
+            Array.Copy(contentBytes, 0, results, myIdx, actualLen);
+            // 指针跳过整个字符区（不足部分自动补0）
+            myIdx += maxCharCount;
+        }
+
+        return results;
+    }
+
+    private static string[] S7ByteArrayToStringArray(byte[] byteArray, byte maxCharCount)
+    {
+        int idx = 0;
+        string[] results = new string[byteArray.Length / (2 + maxCharCount)];
+
+        for (int i = 0; i < byteArray.Length; i += 2 + maxCharCount)
+        {
+            int totalLength = byteArray[i];
+            int contentLength = byteArray[i + 1];
+            results[idx++] = totalLength < contentLength ? "" : S7StringEncoding.GetString(byteArray, i + 2, contentLength);
+        }
+
+        return results;
     }
 }
